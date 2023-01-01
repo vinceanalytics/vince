b4275065209d4c08b191e8c8c5615c911277b5c0
gernest <geofreyernest@live.com>
2023-01-02 01:38:13 +0300

    add referrer parsing
    Pre generate a map for faster runtime lookup.

--- gen.go
+++ gen.go
@@ -1,3 +1,4 @@
 package vince
 
 //go:generate protoc -I=. --go_out=paths=source_relative:. events.proto
+//go:generate go run referrer/make_referrer.go
---
+++ referrer.go
@@ -0,0 +1,28 @@
+package vince
+
+import (
+	"sort"
+	"strings"
+)
+
+func ParseReferrer(host string) *Medium {
+	host = strings.TrimPrefix(host, "www.")
+	parts := strings.Split(host, ".")
+	sort.Sort(sort.Reverse(stringSlice(parts)))
+	if len(parts) > maxReferrerSize {
+		parts = parts[:maxReferrerSize]
+	}
+	for i := len(parts); i >= minReferrerSize; i -= 1 {
+		host = strings.Join(parts[:i], ".")
+		if m, ok := refList[host]; ok {
+			return &Medium{Type: m.Type, Name: m.Name}
+		}
+	}
+	return nil
+}
+
+type stringSlice []string
+
+func (x stringSlice) Len() int           { return len(x) }
+func (x stringSlice) Less(i, j int) bool { return i < j }
+func (x stringSlice) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
