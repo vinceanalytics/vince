8672b0ab2e026145f34e413efc5aa6b5e7ae428f
gernest <geofreyernest@live.com>
2023-01-04 00:16:09 +0300

    add tracker scripts
    This is taken from plausible. A simple find and replace of plausible with vince is done.
    It does not matter the name of the script, but we are avoiding  plausible branding
    since we are competitor.
    
    The plan is to embed the generated js files and serve them from virtual file system.
    This change does not include embedding.

---
+++ tracker/src/vince.js
@@ -0,0 +1,161 @@
+(function(){
+  'use strict';
+
+  var location = window.location
+  var document = window.document
+
+  {{#if compat}}
+  var scriptEl = document.getElementById('vince');
+  {{else}}
+  var scriptEl = document.currentScript;
+  {{/if}}
+  var endpoint = scriptEl.getAttribute('data-api') || defaultEndpoint(scriptEl)
+
+  function warn(reason) {
+    console.warn('Ignoring Event: ' + reason);
+  }
+
+  function defaultEndpoint(el) {
+    {{#if compat}}
+    var pathArray = el.src.split( '/' );
+    var protocol = pathArray[0];
+    var host = pathArray[2];
+    return protocol + '//' + host  + '/api/event';
+    {{else}}
+    return new URL(el.src).origin + '/api/event'
+    {{/if}}
+  }
+
+
+  function trigger(eventName, options) {
+    {{#unless local}}
+    if (/^localhost$|^127(\.[0-9]+){0,2}\.[0-9]+$|^\[::1?\]$/.test(location.hostname) || location.protocol === 'file:') return warn('localhost');
+    if (window._phantom || window.__nightmare || window.navigator.webdriver || window.Cypress) return;
+    {{/unless}}
+    try {
+      if (window.localStorage.vince_ignore === 'true') {
+        return warn('localStorage flag')
+      }
+    } catch (e) {
+
+    }
+    {{#if exclusions}}
+    var dataIncludeAttr = scriptEl && scriptEl.getAttribute('data-include')
+    var dataExcludeAttr = scriptEl && scriptEl.getAttribute('data-exclude')
+
+    if (eventName === 'pageview') {
+      var isIncluded = !dataIncludeAttr || (dataIncludeAttr && dataIncludeAttr.split(',').some(pathMatches))
+      var isExcluded = dataExcludeAttr && dataExcludeAttr.split(',').some(pathMatches)
+
+      if (!isIncluded || isExcluded) return warn('exclusion rule')
+    }
+
+    function pathMatches(wildcardPath) {
+      var actualPath = location.pathname
+
+      {{#if hash}}
+      actualPath += location.hash
+      {{/if}}
+
+      return actualPath.match(new RegExp('^' + wildcardPath.trim().replace(/\*\*/g, '.*').replace(/([^\.])\*/g, '$1[^\\s\/]*') + '\/?$'))
+    }
+    {{/if}}
+
+    var payload = {}
+    payload.n = eventName
+    {{#if manual}}
+    payload.u = options && options.u ? options.u : location.href
+    {{else}}
+    payload.u = location.href
+    {{/if}}
+    payload.d = scriptEl.getAttribute('data-domain')
+    payload.r = document.referrer || null
+    payload.w = window.innerWidth
+    if (options && options.meta) {
+      payload.m = JSON.stringify(options.meta)
+    }
+    if (options && options.props) {
+      payload.p = options.props
+    }
+
+    {{#if dimensions}}
+    var dimensionAttributes = scriptEl.getAttributeNames().filter(function (name) {
+      return name.substring(0, 6) === 'event-'
+    })
+
+    var props = payload.p || {}
+
+    dimensionAttributes.forEach(function(attribute) {
+      var propKey = attribute.replace('event-', '')
+      var propValue = scriptEl.getAttribute(attribute)
+      props[propKey] = props[propKey] || propValue
+    })
+
+    payload.p = props
+    {{/if}}
+
+    {{#if hash}}
+    payload.h = 1
+    {{/if}}
+
+    var request = new XMLHttpRequest();
+    request.open('POST', endpoint, true);
+    request.setRequestHeader('Content-Type', 'text/plain');
+
+    request.send(JSON.stringify(payload));
+
+    request.onreadystatechange = function() {
+      if (request.readyState === 4) {
+        options && options.callback && options.callback()
+      }
+    }
+  }
+
+  var queue = (window.vince && window.vince.q) || []
+  window.vince = trigger
+  for (var i = 0; i < queue.length; i++) {
+    trigger.apply(this, queue[i])
+  }
+
+  {{#unless manual}}
+    var lastPage;
+
+    function page() {
+      {{#unless hash}}
+      if (lastPage === location.pathname) return;
+      {{/unless}}
+      lastPage = location.pathname
+      trigger('pageview')
+    }
+
+    {{#if hash}}
+    window.addEventListener('hashchange', page)
+    {{else}}
+    var his = window.history
+    if (his.pushState) {
+      var originalPushState = his['pushState']
+      his.pushState = function() {
+        originalPushState.apply(this, arguments)
+        page();
+      }
+      window.addEventListener('popstate', page)
+    }
+    {{/if}}
+
+    function handleVisibilityChange() {
+      if (!lastPage && document.visibilityState === 'visible') {
+        page()
+      }
+    }
+
+    if (document.visibilityState === 'prerender') {
+      document.addEventListener('visibilitychange', handleVisibilityChange);
+    } else {
+      page()
+    }
+  {{/unless}}
+
+  {{#if (any outbound_links file_downloads tagged_events)}}
+  {{> customEvents}}
+  {{/if}}
+})();
---
+++ tracker/test/custom-event-edge-cases.spec.js
@@ -0,0 +1,40 @@
+const { test } = require('./support/harness');
+const { mockRequest, mockManyRequests, expectCustomEvent } = require('./support/test-utils');
+const { expect } = require('@playwright/test');
+const { LOCAL_SERVER_ADDR } = require('./support/server');
+
+
+test.describe('script.file-downloads.outbound-links.tagged-events.js', () => {
+    test('sends only outbound link event when clicked link is both download and outbound', async ({ page }) => {
+        await page.goto('/custom-event-edge-case.html')
+        const downloadURL = await page.locator('#outbound-download-link').getAttribute('href')
+
+        const vinceRequestMockList = mockManyRequests(page, '/api/event', 2)
+        await page.click('#outbound-download-link')
+
+        const requests = await vinceRequestMockList
+        expect(requests.length).toBe(1)
+        expectCustomEvent(requests[0], 'Outbound Link: Click', { url: downloadURL })
+    });
+
+    test('sends file download event when local download link clicked', async ({ page }) => {
+        await page.goto('/custom-event-edge-case.html')
+        const downloadURL = LOCAL_SERVER_ADDR + '/' + await page.locator('#local-download').getAttribute('href')
+
+        const vinceRequestMock = mockRequest(page, '/api/event')
+        await page.click('#local-download')
+
+        expectCustomEvent(await vinceRequestMock, 'File Download', { url: downloadURL })
+    });
+
+    test('sends only tagged event when clicked link is tagged + outbound + download', async ({ page }) => {
+        await page.goto('/custom-event-edge-case.html')
+
+        const vinceRequestMockList = mockManyRequests(page, '/api/event', 3)
+        await page.click('#tagged-outbound-download-link')
+
+        const requests = await vinceRequestMockList
+        expect(requests.length).toBe(1)
+        expectCustomEvent(requests[0], 'Foo', {})
+    });
+});
