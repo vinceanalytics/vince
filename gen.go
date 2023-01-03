4d9473a55d5824d253913da057b587b471259631
gernest <geofreyernest@live.com>
2023-01-03 16:12:26 +0300

    ua: generate client ua

--- gen.go
+++ gen.go
@@ -3,3 +3,4 @@ package vince
 //go:generate protoc -I=. --go_out=paths=source_relative:. events.proto
 //go:generate go run ua/bot/make_bot.go
 //go:generate go run ua/device/make_device.go
+//go:generate go run ua/client/make_client.go
