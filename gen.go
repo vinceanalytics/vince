d6cc6c76bc57c3b13402b256d94ffc33369ab7e4
gernest <geofreyernest@live.com>
2023-01-03 16:52:28 +0300

    ua: generate ua_os.go

--- gen.go
+++ gen.go
@@ -4,3 +4,4 @@ package vince
 //go:generate go run ua/bot/make_bot.go
 //go:generate go run ua/device/make_device.go
 //go:generate go run ua/client/make_client.go
+//go:generate go run ua/os/make_os.go
