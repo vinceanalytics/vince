81d67e416211442b6fe89ed95b2e9518977b48cb
gernest <geofreyernest@live.com>
2023-01-03 17:15:29 +0300

    ua: generate ua_vendor.go

--- gen.go
+++ gen.go
@@ -5,3 +5,4 @@ package vince
 //go:generate go run ua/device/make_device.go
 //go:generate go run ua/client/make_client.go
 //go:generate go run ua/os/make_os.go
+//go:generate go run ua/vendor_fragment/make_vendor.go
