a015380b3574b02c210288368e09cee10d005804
gernest <geofreyernest@live.com>
2023-03-24 13:35:49 +0300

    remove unused proto

--- timeseries/timeseries.go
+++ timeseries/timeseries.go
@@ -1,4 +1,4 @@
 package timeseries
 
 //go:generate protoc -I=. --go_out=paths=source_relative:. event.proto
-//go:generate go run golang.org/x/tools/go/analysis/passes/fieldalignment/cmd/fieldalignment -fix .
+//go:generate go run golang.org/x/tools/go/analysis/passes/fieldalignment/cmd/fieldalignment -fix  .
