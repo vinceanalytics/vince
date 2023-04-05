75540da490f56b3f5edf73ea8dd64ff97060ff49
gernest <geofreyernest@live.com>
2023-04-05 21:51:55 +0300

    index regions

---
+++ cities/cities.go
@@ -0,0 +1,9 @@
+package cities
+
+//go:generate go run index/main.go
+
+// You need to set CITIES_DATA env var to point to
+// https://github.com/dr5hn/countries-states-cities-database/blob/master/countries%2Bstates%2Bcities.json
+// before executing go generate
+// example
+//	CITIES_DATA=~/Downloads/countries+states+cities.json  go generate ./cities/
