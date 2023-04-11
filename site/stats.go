bda2c4664ff0461fca6cb09217ab1ec443499051
gernest <geofreyernest@live.com>
2023-04-11 13:44:52 +0300

    initial work on stats page

--- site/stats.go
+++ site/stats.go
@@ -3,9 +3,20 @@ package site
 import (
 	"net/http"
 
+	"github.com/gernest/vince/models"
 	"github.com/gernest/vince/render"
 )
 
 func Stats(w http.ResponseWriter, r *http.Request) {
-	render.ERROR(r.Context(), w, http.StatusNotImplemented)
+	ctx := r.Context()
+	site := models.GetSite(ctx)
+	role := models.GetRole(ctx)
+	canSeeStats := !site.Locked || role == "super_admin"
+	switch {
+	case !site.StatsStartDate.IsZero() && canSeeStats:
+	case site.StatsStartDate.IsZero() && canSeeStats:
+	case site.Locked:
+	default:
+		render.ERROR(r.Context(), w, http.StatusNotImplemented)
+	}
 }
