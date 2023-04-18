97b10735310044871964fb19f63553e4c4728c3b
gernest <geofreyernest@live.com>
2023-04-18 14:19:44 +0300

    working delete site

--- sites/delete.go
+++ sites/delete.go
@@ -3,34 +3,9 @@ package sites
 import (
 	"net/http"
 
-	"github.com/gernest/vince/caches"
-	"github.com/gernest/vince/flash"
-	"github.com/gernest/vince/models"
-	"github.com/gernest/vince/sessions"
-	"github.com/gernest/vince/timeseries"
+	"github.com/gernest/vince/render"
 )
 
 func Delete(w http.ResponseWriter, r *http.Request) {
-	ctx := r.Context()
-	site := models.GetSite(ctx)
-	u := models.GetUser(ctx)
-
-	// remove site from database
-	err := models.Get(ctx).Delete(site).Error
-	if err != nil {
-		models.DBE(ctx, err, "failed to delete site")
-	}
-	// remove site from cache
-	caches.Site(ctx).Del(site.Domain)
-
-	// remove site events in collection  buffers
-	timeseries.GetMap(ctx).Delete(ctx, site.ID)
-
-	// permanently remove site stats
-	timeseries.DropSite(ctx, u.ID, site.ID)
-	session, r := sessions.Load(r)
-	session.Data.Flash = &flash.Flash{
-		Success: []string{"site and site stats have been permanently deleted"},
-	}
-	http.Redirect(w, r, "/sites", http.StatusFound)
+	render.ERROR(r.Context(), w, http.StatusNotImplemented)
 }
