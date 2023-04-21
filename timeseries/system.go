2caa8943b024f9272b418791a3552540ab5c44bc
gernest <geofreyernest@live.com>
2023-04-21 20:46:03 +0300

    sort stats in ascending order

--- timeseries/system.go
+++ timeseries/system.go
@@ -33,7 +33,7 @@ func NewSystem[T any](dir, name string) (*System[T], error) {
 		w: parquet.NewSortingWriter[T](f, 64<<10,
 			parquet.SortingWriterConfig(
 				parquet.SortingColumns(
-					parquet.Descending("timestamp"),
+					parquet.Ascending("timestamp"),
 				),
 			),
 		),
