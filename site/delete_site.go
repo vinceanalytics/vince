fb6323902cd6f304e5f510e106d23812ae0e8cd8
gernest <geofreyernest@live.com>
2023-04-22 20:45:31 +0300

    chore: use session.SuccessFlash

--- site/delete_site.go
+++ site/delete_site.go
@@ -4,7 +4,6 @@ import (
 	"net/http"
 
 	"github.com/gernest/vince/caches"
-	"github.com/gernest/vince/flash"
 	"github.com/gernest/vince/models"
 	"github.com/gernest/vince/sessions"
 	"github.com/gernest/vince/timeseries"
@@ -26,10 +25,8 @@ func DeleteSite(w http.ResponseWriter, r *http.Request) {
 
 	// permanently remove site stats
 	timeseries.DropSite(ctx, u.ID, site.ID)
+
 	session, r := sessions.Load(r)
-	session.Data.Flash = &flash.Flash{
-		Success: []string{"site and site stats have been permanently deleted"},
-	}
-	session.Save(w)
+	session.SuccessFlash("site and site stats have been permanently deleted").Save(w)
 	http.Redirect(w, r, "/sites", http.StatusFound)
 }
