7050f5e63519730e5a82940c677975a9f04b12bb
gernest <geofreyernest@live.com>
2023-04-30 19:30:12 +0300

    add GET /api/:domain/status

--- api/domain_statuus.go
+++ api/domain_statuus.go
@@ -3,9 +3,21 @@ package api
 import (
 	"net/http"
 
+	"github.com/gernest/vince/models"
+	"github.com/gernest/vince/params"
 	"github.com/gernest/vince/render"
 )
 
 func DomainStatus(w http.ResponseWriter, r *http.Request) {
-	render.ERROR(r.Context(), w, http.StatusNotImplemented)
+	ctx := r.Context()
+	site := models.SiteByDomain(ctx, params.Get(ctx)["domain"])
+	if site != nil {
+		status := "waiting"
+		if site.HasStats {
+			status = "ready"
+		}
+		render.JSON(w, http.StatusOK, status)
+		return
+	}
+	http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
 }
