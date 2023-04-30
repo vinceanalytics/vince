d86400d97613efb7e898ed79518eade480a074a6
gernest <geofreyernest@live.com>
2023-04-30 22:50:56 +0300

    pass context.Context to app level goroutine

--- pkg/group/group.go
+++ pkg/group/group.go
@@ -12,6 +12,8 @@ func Set(ctx context.Context, g *errgroup.Group) context.Context {
 	return context.WithValue(ctx, groupKey{}, g)
 }
 
-func GO(ctx context.Context, f func() error) {
-	ctx.Value(groupKey{}).(*errgroup.Group).Go(f)
+func GO(ctx context.Context, f func(context.Context) error) {
+	ctx.Value(groupKey{}).(*errgroup.Group).Go(func() error {
+		return f(ctx)
+	})
 }
