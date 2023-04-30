70d2c8891671f77de1ec1c34266ef8728ab5e839
gernest <geofreyernest@live.com>
2023-04-30 19:12:40 +0300

    remove /api/error

--- api/error.go
+++
@@ -1,7 +0,0 @@
-package api
-
-import (
-	"net/http"
-)
-
-func Error(w http.ResponseWriter, r *http.Request) {}
--- router/router.go
+++ router/router.go
@@ -47,7 +47,6 @@ func Pipe(ctx context.Context) plug.Pipeline {
 
 		plug.PREFIX("/api/",
 			pipe4.PathPOST("/api/event", api.Events),
-			pipe4.PathGET("/api/error", api.Error),
 			pipe4.PathGET("/api/health", api.Health),
 			pipe4.PathGET("/api/system", api.Info),
 			pipe4.PathGET("/api/sites", api.Sites),
