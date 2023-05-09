ef332bdfcbcd2fdf90def2b3fa545e972bff6e28
gernest <geofreyernest@live.com>
2023-05-09 11:21:24 +0300

    add favicon and meta tags

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,17 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   description: "The Cloud Native Web Analytics Platform",
   title: "- Cloud Native Web Analytics",
+  head: [
+    ["meta", { name: "msapplication-TileColor", content: "#bdfcff" }],
+    ["meta", { name: "twitter:card", content: "product" }],
+    ["meta", { name: "twitter:site", content: "@gernesti" }],
+    ["meta", { name: "twitter:title", content: "Vince analytics" }],
+    ["meta", { name: "twitter:description", content: "The Cloud Native Web Analytics Platform." }],
+    ["meta", { name: "og:url", content: "https://vinceanalytics.com" }],
+    ["meta", { name: "og:title", content: "Vince Analytics" }],
+    ["meta", { name: "og:description", content: "The Cloud Native Web Analytics Platform." }],
+    ["meta", { name: "og:type", content: "article" }],
+  ],
   themeConfig: {
     logo: '/logo.svg',
     // https://vitepress.dev/reference/default-theme-config
