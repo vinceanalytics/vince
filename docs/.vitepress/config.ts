0f3493a5bb6f71017f1719861c69bdf26aed912e
gernest <geofreyernest@live.com>
2023-05-19 20:08:47 +0300

    docs: setup structure of pages

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -27,13 +27,99 @@ export default defineConfig({
     sidebar: [
       {
         text: 'Getting Started',
+        collapsed: true,
         items: [
           { text: 'Installation', link: '/guide/install-vince' },
           { text: 'Cli', link: '/guide/cli-vince' },
         ]
       },
       {
+        text: "Site Settings",
+        collapsed: true,
+        items: [
+          {
+            text: "Site Landing Page",
+          },
+          {
+            text: "Site Setting",
+          },
+          {
+            text: "Change Domain Name",
+          },
+          {
+            text: "Invite team members,assign roles and remove users",
+          },
+          {
+            text: "Open site to the public",
+          },
+          {
+            text: "Share your stats with a private and secure link",
+          },
+          {
+            text: "Send reports via email",
+          },
+          {
+            text: "Alerts",
+            items: [],
+          },
+          {
+            text: "Exclude pages from being tracked",
+          },
+          {
+            text: "Transfer ownership of a site",
+          },
+          {
+            text: "Reset your site data",
+          },
+          {
+            text: "Delete your site data and stats",
+          },
+        ],
+      },
+      {
+        text: "Stats Dashboard",
+        collapsed: true,
+        items: [],
+      },
+      {
+        text: "Goals and Custom EVents",
+        collapsed: true,
+        items: [],
+      },
+      {
+        text: "API",
+        collapsed: true,
+        items: [
+          {
+            text: "Stats API reference"
+          },
+          {
+            text: "Events API reference"
+          },
+          {
+            text: "Site Provisioning API reference"
+          },
+        ],
+      },
+      {
+        text: "Account Settings",
+        collapsed: true,
+        items: [
+          {
+            text: "Change your account email address"
+          },
+          {
+            text: "Reset your account password"
+          },
+          {
+            text: "Delete your account"
+          },
+        ],
+      },
+
+      {
         text: 'Kubernetes',
+        collapsed: true,
         items: [
           {
             text: 'Installation', items: [
@@ -41,10 +127,18 @@ export default defineConfig({
                 text: 'Out of cluster',
                 link: '/guide/install-v8s'
               },
+              {
+                text: 'Using helm',
+              },
             ]
           },
           { text: 'Cli', link: '/guide/cli-v8s' },
         ]
+      },
+      {
+        text: "Contribute",
+        collapsed: true,
+        items: [],
       },
     ],
 
