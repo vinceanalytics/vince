d4df4b3dc945501ebbc6d19fd91370f36542a65f
gernest <geofreyernest@live.com>
2023-05-21 11:19:50 +0300

    add delete api test

--- pkg/k8s/client_test.go
+++ pkg/k8s/client_test.go
@@ -46,6 +46,39 @@ func TestSiteAPI(t *testing.T) {
 			t.Errorf(" ==>\n EXPECTED \n%s\n GOT\n%s", string(expect), string(got))
 		}
 	})
+	t.Run("Delete", func(t *testing.T) {
+		r := cloneTransport{}
+		a := siteAPI{
+			client: http.Client{
+				Transport: &r,
+			},
+		}
+		secret := v1.Secret{
+			ObjectMeta: metav1.ObjectMeta{
+				Name:      "vince",
+				Namespace: "default",
+			},
+			Data: map[string][]byte{
+				secrets.API_KEY: []byte("secret"),
+			},
+		}
+		err := a.Delete(context.TODO(), &secret, "example.com")
+		if err != nil {
+			t.Fatal(err)
+		}
+		expect, err := os.ReadFile("./testdata/delete_api")
+		if err != nil {
+			t.Fatal(err)
+		}
+		got, err := httputil.DumpRequestOut(r.r, true)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !bytes.Equal(expect, got) {
+			t.Errorf(" ==>\n EXPECTED \n%s\n GOT\n%s", string(expect), string(got))
+		}
+	})
+
 }
 
 var _ http.RoundTripper = (*cloneTransport)(nil)
