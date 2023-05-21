f41db26329612432c25c39813679378c8aae32b9
gernest <geofreyernest@live.com>
2023-05-21 14:11:58 +0300

    update version

--- cmd/version/version.go
+++ cmd/version/version.go
@@ -1,13 +1,9 @@
 package version
 
 import (
-	"bytes"
-	"fmt"
 	"os"
-	"runtime/debug"
-	"text/tabwriter"
-	"text/template"
 
+	"github.com/gernest/vince/pkg/version"
 	"github.com/urfave/cli/v3"
 )
 
@@ -16,27 +12,9 @@ func Version() *cli.Command {
 		Name:  "version",
 		Usage: "prints version information",
 		Action: func(ctx *cli.Context) error {
-			build, ok := debug.ReadBuildInfo()
-			if !ok {
-				return nil
-			}
-			var b bytes.Buffer
-			tab := tabwriter.NewWriter(&b, 0, 0, 1, ' ', 0)
-			fmt.Fprintf(tab, "Goversion\t%s\t\n", build.GoVersion)
-			fmt.Fprintf(tab, "Package\t%s\t\n", build.Path)
-			fmt.Fprintf(tab, "Version\t%s\t\n", build.Main.Version)
-			for _, v := range build.Settings {
-				if v.Value != "" {
-					fmt.Fprintf(tab, "%s\t%s\t\n", v.Key, v.Value)
-				}
-			}
-			tab.Flush()
-			os.Stdout.Write(b.Bytes())
+			x := version.Build()
+			os.Stdout.WriteString(x.String())
 			return nil
 		},
 	}
 }
-
-var tpl = template.Must(template.New("v").Parse(`
-GoVersion: 
-`))
---
+++ pkg/version/version.go
@@ -0,0 +1,60 @@
+package version
+
+import (
+	"runtime/debug"
+	"strings"
+)
+
+var (
+	BuildVersion = "0.0.0"
+)
+
+type Version struct {
+	Commit string
+	Date   string
+	Dirty  bool
+	Valid  bool
+}
+
+func (v *Version) String() string {
+	var s strings.Builder
+	s.WriteString("v")
+	s.WriteString(strings.TrimSpace(BuildVersion))
+	if !v.Valid {
+		s.WriteString("-ERR-BuildInfo")
+	} else {
+		s.WriteString(v.Date)
+		commit := v.Commit
+		if len(commit) > 9 {
+			commit = commit[:9]
+		}
+		s.WriteString("-" + commit)
+		if v.Dirty {
+			s.WriteString("-dirty")
+		}
+	}
+	s.WriteByte('\n')
+	return s.String()
+}
+
+func Build() Version {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return Version{}
+	}
+	v := Version{Valid: true}
+	for _, s := range bi.Settings {
+		switch s.Key {
+		case "vcs.revision":
+			v.Commit = s.Value
+		case "vcs.time":
+			if len(s.Value) >= len("yyyy-mm-dd") {
+				v.Date = s.Value[:len("yyyy-mm-dd")]
+				v.Date = strings.ReplaceAll(v.Date, "-", "")
+			}
+		case "vcs.modified":
+			v.Dirty = s.Value == "true"
+		}
+	}
+	return v
+}
