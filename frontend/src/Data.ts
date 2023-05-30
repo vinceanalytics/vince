a1f9d30bf79bcbd933e4cc9e6cc42a66ebd89b59
gernest <geofreyernest@live.com>
2023-05-31 00:49:17 +0300

    define new data interface

--- frontend/src/Data.ts
+++ frontend/src/Data.ts
@@ -1,66 +1,65 @@
-export interface Data {
-    timestamps?: number[];
-    all?: Aggregate;
-    Event?: EntryMap;
-    page?: EntryMap;
-    entryPage?: EntryMap;
-    exitPage?: EntryMap;
-    referrer?: EntryMap;
-    utmMedium?: EntryMap;
-    utmSource?: EntryMap;
-    utmCampaign?: EntryMap;
-    utmContent?: EntryMap;
-    utmTerm?: EntryMap;
-    utmDevice?: EntryMap;
-    utmBrowser?: EntryMap;
-    utmBrowserVersion?: EntryMap;
-    os?: EntryMap;
-    osVersion?: EntryMap;
-    country?: EntryMap;
-    region?: EntryMap;
-    city?: EntryMap;
+export type Property =
+    | "base"
+    | "event"
+    | "page"
+    | "entryPage"
+    | "exitPage"
+    | "referrer"
+    | "UtmMedium"
+    | "UtmSource"
+    | "UtmCampaign"
+    | "UtmContent"
+    | "UtmTerm"
+    | "UtmDevice"
+    | "UtmBrowser"
+    | "browserVersion"
+    | "os"
+    | "osVersion"
+    | "country"
+    | "region"
+    | "city";
+
+export type Metric =
+    | "visitors"
+    | "views"
+    | "events"
+    | "visits"
+    | "bounceRate"
+    | "visitDuration"
+    | "viewsPerVisit";
+
+export interface Query {
+    range: Range;
+    metrics: Metric[];
+    prop: Property;
+    match: Match;
 }
 
-export interface EntryMap {
-    entries?: Entries;
-    sum?: Summary;
-    percent?: Summary,
+export interface Range {
+    from: Date;
+    to: Date;
 }
 
-export interface Entries {
-    [key: string]: AggregateEntry;
+export interface Match {
+    text: string;
+    isRe: boolean;
 }
 
-export interface Summary {
-    visitors?: Item[];
-    views?: Item[];
-    events?: Item[];
-    visits?: Item[];
-    bounceRate?: Item[];
-    visitDuration?: Item[];
-    viewsPerVisit?: Item[];
+export interface QueryResult {
+    elapsed: string;
+    result: Result[];
 }
 
-export interface Item {
-    key: string;
-    value: string;
+export interface Result {
+    metric: Metric;
+    values: Values;
 }
 
-export interface AggregateEntry {
-    aggregate: Aggregate,
+export interface Values {
+    [key: string]: Value[]
 }
 
-export interface Aggregate {
-    visitors?: Entry;
-    views?: Entry;
-    events?: Entry;
-    visits?: Entry;
-    bounceRate?: Entry;
-    visitDuration?: Entry;
-    viewsPerVisit?: Entry;
-}
-
-export interface Entry {
-    sum: number;
-    values: number[];
+export interface Value {
+    timestamp: number;
+    value: number;
 }
