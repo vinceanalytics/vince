35c93b72ba9aadd9b4355fe48ef36c726f953730
gernest <geofreyernest@live.com>
2023-06-03 20:58:43 +0300

    chore: build frontend from frontend path

--- frontend/build.js
+++ frontend/build.js
@@ -4,30 +4,30 @@ const { compile } = pkg;
 import { writeFileSync, copyFileSync } from "fs";
 
 const sharedConfig = {
-    entryPoints: ["frontend/src/index.ts"],
+    entryPoints: ["src/index.ts"],
     bundle: true,
     minify: true,
 };
 
 build({
     ...sharedConfig,
-    outfile: "assets/js/index.js",
+    outfile: "../assets/js/index.js",
     keepNames: true,
 });
 
-const result = compile("frontend/scss/main.scss", {
+const result = compile("scss/main.scss", {
     style: "compressed",
 })
 
-writeFileSync("assets/css/app.css", result.css);
+writeFileSync("../assets/css/app.css", result.css);
 
 // copy styles
-copyFileSync("node_modules/@primer/css/dist/primer.css", "assets/css/primer.css")
-copyFileSync("node_modules/@primer/css/dist/primer.css.map", "assets/css/primer.css.map")
-copyFileSync("node_modules/@primer/view-components/app/assets/styles/primer_view_components.css", "assets/css/primer_view_components.css")
-copyFileSync("node_modules/@primer/view-components/app/assets/styles/primer_view_components.css.map", "assets/css/primer_view_components.css.map")
+copyFileSync("node_modules/@primer/css/dist/primer.css", "../assets/css/primer.css")
+copyFileSync("node_modules/@primer/css/dist/primer.css.map", "../assets/css/primer.css.map")
+copyFileSync("node_modules/@primer/view-components/app/assets/styles/primer_view_components.css", "../assets/css/primer_view_components.css")
+copyFileSync("node_modules/@primer/view-components/app/assets/styles/primer_view_components.css.map", "../assets/css/primer_view_components.css.map")
 
 
 //copy js
-copyFileSync("node_modules/@primer/view-components/app/assets/javascripts/primer_view_components.js", "assets/js/primer_view_components.js")
-copyFileSync("node_modules/@primer/view-components/app/assets/javascripts/primer_view_components.js.map", "assets/js/primer_view_components.js.map")
+copyFileSync("node_modules/@primer/view-components/app/assets/javascripts/primer_view_components.js", "../assets/js/primer_view_components.js")
+copyFileSync("node_modules/@primer/view-components/app/assets/javascripts/primer_view_components.js.map", "../assets/js/primer_view_components.js.map")
