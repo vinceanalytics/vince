53969e03ad0e04804f7c1235c035a89d79222a7d
gernest <geofreyernest@live.com>
2023-06-06 17:47:23 +0300

    chore: add tests.Drive
    This executes vince_load_gen and collects its output

---
+++ tests/tests.go
@@ -0,0 +1,40 @@
+package tests
+
+import (
+	"bytes"
+	"os/exec"
+	"strings"
+	"sync"
+	"testing"
+
+	"github.com/vinceanalytics/vince/tools"
+)
+
+var once sync.Once
+
+const driveBinary = "vince_load_gen"
+
+// Returns the binary for vince_load_gen
+func binary() string {
+	once.Do(func() {
+		tools.ExecPlainWithWorkingPath(
+			tools.RootVince(),
+			"go", "install",
+			"./tools/vince_load_gen",
+		)
+	})
+	return driveBinary
+}
+
+func Drive(t *testing.T, script string, f ...func(*exec.Cmd)) string {
+	t.Helper()
+	var o bytes.Buffer
+	cmd := exec.Command(binary(), script)
+	cmd.Stderr = &o
+	cmd.Stdout = &o
+	err := cmd.Run()
+	if err != nil {
+		t.Fatalf("failed executing driver %s", cmd)
+	}
+	return strings.TrimSpace(o.String())
+}
