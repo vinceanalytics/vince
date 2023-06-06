c9aab3628b59ecd99af05c07ef085a5039f88405
gernest <geofreyernest@live.com>
2023-06-06 18:00:15 +0300

    chore: add tests.Host

--- tests/tests.go
+++ tests/tests.go
@@ -2,6 +2,7 @@ package tests
 
 import (
 	"bytes"
+	"fmt"
 	"os/exec"
 	"strings"
 	"sync"
@@ -37,4 +38,11 @@ func Drive(t *testing.T, script string, f ...func(*exec.Cmd)) string {
 		t.Fatalf("failed executing driver %s", cmd)
 	}
 	return strings.TrimSpace(o.String())
+}
+
+// Host is like Drive but customize the target host for the driver
+func Host(t *testing.T, host, script string) string {
+	return Drive(t, script, func(c *exec.Cmd) {
+		c.Env = append(c.Env, fmt.Sprintf("VLG_HOST=%s", host))
+	})
 }
