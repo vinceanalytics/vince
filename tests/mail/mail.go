33d60beb2e402e885d83be7976e4e25822d3b00a
gernest <geofreyernest@live.com>
2023-06-08 20:07:19 +0300

    add tests/mail package
    This allows testing features with smtp email submission.

---
+++ tests/mail/mail.go
@@ -0,0 +1,82 @@
+package mail
+
+import (
+	"io"
+	"io/ioutil"
+	"time"
+
+	"github.com/emersion/go-smtp"
+)
+
+var _ smtp.Backend = (*Backend)(nil)
+var _ smtp.Session = (*Session)(nil)
+
+type Backend struct {
+	*smtp.Server
+	Msg       map[string]Message
+	OnMessage func(*Message)
+}
+
+func New(addr string) *Backend {
+	be := &Backend{Msg: make(map[string]Message)}
+	be.Server = smtp.NewServer(be)
+	be.Addr = addr
+	be.Domain = "localhost"
+	be.ReadTimeout = 10 * time.Second
+	be.WriteTimeout = 10 * time.Second
+	be.MaxMessageBytes = 1024 * 1024
+	be.MaxRecipients = 50
+	be.AllowInsecureAuth = true
+	return be
+}
+
+func (b *Backend) NewSession(_ *smtp.Conn) (smtp.Session, error) {
+	return &Session{b: b}, nil
+}
+
+type Session struct {
+	b   *Backend
+	msg Message
+}
+
+func (s *Session) AuthPlain(username, password string) error {
+	return nil
+}
+
+func (s *Session) Mail(from string, opts *smtp.MailOptions) error {
+	s.msg.From = from
+	return nil
+}
+
+func (s *Session) Rcpt(to string) error {
+	s.msg.To = append(s.msg.To, to)
+	return nil
+}
+
+func (s *Session) Data(r io.Reader) error {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return err
+	}
+	s.msg.Data = string(b)
+	s.b.Msg[s.msg.From] = s.msg
+	if s.b.OnMessage != nil {
+		s.b.OnMessage(&s.msg)
+	}
+	s.msg = Message{}
+	return nil
+}
+
+func (s *Session) Reset() {
+	s.msg = Message{}
+}
+
+func (s *Session) Logout() error {
+	return nil
+}
+
+type Message struct {
+	From string
+	To   []string
+	Data string
+}
