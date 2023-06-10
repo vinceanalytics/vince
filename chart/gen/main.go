c1d765bd990e7959ad44645a47d37e37faef16c0
gernest <geofreyernest@live.com>
2023-06-10 23:01:00 +0300

    chore: include crds in kustomize yaml

--- chart/gen/main.go
+++ chart/gen/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"os"
 	"path/filepath"
 
 	"github.com/vinceanalytics/vince/tools"
@@ -10,8 +12,17 @@ func main() {
 	tools.ExecPlain(
 		"helm", "package", ".", "-d", "charts",
 	)
-	o := tools.ExecCollect("helm", "template", ".")
+	var b bytes.Buffer
+	fs, err := os.ReadDir("crds")
+	if err != nil {
+		tools.Exit(err.Error())
+	}
+	for _, f := range fs {
+		b.Write(tools.ReadFile(filepath.Join("crds", f.Name())))
+	}
+	b.WriteString(tools.ExecCollect("helm", "template", "."))
+
 	dir := filepath.Join(tools.RootVince(), "website/docs/.vitepress/dist/kustomize")
 	tools.MkDir(dir)
-	tools.WriteFile(filepath.Join(dir, "v8s.yaml"), []byte(o))
+	tools.WriteFile(filepath.Join(dir, "v8s.yaml"), b.Bytes())
 }
