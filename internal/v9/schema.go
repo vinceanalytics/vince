a1a338bbd63d982cadfc485e80ce165265e76beb
gernest <geofreyernest@live.com>
2023-07-06 20:13:31 +0300

    assign each site its own table

--- internal/v9/schema.go
+++ internal/v9/schema.go
@@ -4,55 +4,93 @@ import (
 	"context"
 	"errors"
 	"path/filepath"
+	"sync"
 
 	"github.com/polarsignals/frostdb"
 	"github.com/polarsignals/frostdb/dynparquet"
-	schemapb "github.com/polarsignals/frostdb/gen/proto/go/frostdb/schema/v1alpha1"
+	schemapb "github.com/polarsignals/frostdb/gen/proto/go/frostdb/schema/v1alpha2"
 	"github.com/segmentio/parquet-go"
 	"github.com/vinceanalytics/vince/pkg/entry"
 	"github.com/vinceanalytics/vince/pkg/spec"
 )
 
 type V9 struct {
-	store         *frostdb.ColumnStore
-	db            *frostdb.DB
-	table         *frostdb.Table
+	store *frostdb.ColumnStore
+	db    *frostdb.DB
+}
+
+type rowsBuffer struct {
 	buf           *dynparquet.Buffer
 	rows          []parquet.Row
 	maxBufferSize int
 }
 
-type SaveAggregateFunc func(ctx context.Context, ts int64, uid, sid uint64, p spec.Property, key string, sum *entry.Aggregate) error
-
-func (v *V9) Aggregate(ctx context.Context, ts int64, uid, sid uint64, p spec.Property, key string, sum *entry.Aggregate) error {
-	return errors.Join(
-		v.Add(ctx, ts, uid, sid, p, spec.Visitors, key, int64(sum.Visitors)),
-		v.Add(ctx, ts, uid, sid, p, spec.Views, key, int64(sum.Views)),
-		v.Add(ctx, ts, uid, sid, p, spec.Events, key, int64(sum.Events)),
-		v.Add(ctx, ts, uid, sid, p, spec.Visits, key, int64(sum.Visits)),
-		v.Add(ctx, ts, uid, sid, p, spec.BounceRates, key, int64(sum.BounceRates)),
-		v.Add(ctx, ts, uid, sid, p, spec.VisitDurations, key, int64(sum.VisitDurations)),
-	)
+func (r *rowsBuffer) reset() {
+	r.buf.Reset()
+	r.rows = r.rows[:0]
 }
 
-func (v *V9) Do(ctx context.Context, f func(SaveAggregateFunc) error) error {
-	return errors.Join(
-		f(v.Aggregate),
-		v.save(ctx),
-	)
+func (r *rowsBuffer) release() {
+	r.reset()
+	rowsBufferPool.Put(r)
 }
 
-func (v *V9) Add(ctx context.Context, ts int64, uid, sid uint64, prop spec.Property, metric spec.Metric, key string, value int64) error {
-	if len(v.rows) > v.maxBufferSize {
-		err := v.save(ctx)
+func newRowsBuffer() *rowsBuffer {
+	return rowsBufferPool.Get().(*rowsBuffer)
+}
+
+var rowsBufferPool = &sync.Pool{
+	New: func() any {
+		return &rowsBuffer{
+			maxBufferSize: 1 << 10,
+			buf:           must(schema.NewBufferV2()),
+			rows:          make([]parquet.Row, 0, 1<<10),
+		}
+	},
+}
+
+type SaveAggregateFunc func(ctx context.Context, ts int64, p spec.Property, key string, sum *entry.Aggregate) error
+
+func (v *V9) agg(table *frostdb.Table, r *rowsBuffer) SaveAggregateFunc {
+	return func(ctx context.Context, ts int64, p spec.Property, key string, sum *entry.Aggregate) error {
+		return errors.Join(
+			v.Add(ctx, table, r, ts, p, spec.Visitors, key, int64(sum.Visitors)),
+			v.Add(ctx, table, r, ts, p, spec.Views, key, int64(sum.Views)),
+			v.Add(ctx, table, r, ts, p, spec.Events, key, int64(sum.Events)),
+			v.Add(ctx, table, r, ts, p, spec.Visits, key, int64(sum.Visits)),
+			v.Add(ctx, table, r, ts, p, spec.BounceRates, key, int64(sum.BounceRates)),
+			v.Add(ctx, table, r, ts, p, spec.VisitDurations, key, int64(sum.VisitDurations)),
+		)
+	}
+}
+
+func (v *V9) Do(ctx context.Context, domain string, f func(SaveAggregateFunc) error) error {
+	table, err := v.db.GetTable(domain)
+	if err != nil {
+		// Only a single error is returned. We are sure that err is for the missing
+		// table.
+		table, err = v.db.Table(domain, frostdb.NewTableConfig(scheme))
 		if err != nil {
 			return err
 		}
 	}
-	v.rows = append(v.rows, parquet.Row{
+	r := newRowsBuffer()
+	defer r.release()
+	return errors.Join(
+		f(v.agg(table, r)),
+		v.save(ctx, table, r),
+	)
+}
+
+func (v *V9) Add(ctx context.Context, table *frostdb.Table, r *rowsBuffer, ts int64, prop spec.Property, metric spec.Metric, key string, value int64) error {
+	if len(r.rows) >= r.maxBufferSize {
+		err := v.save(ctx, table, r)
+		if err != nil {
+			return err
+		}
+	}
+	r.rows = append(r.rows, parquet.Row{
 		parquet.Int64Value(ts),
-		parquet.Int64Value(int64(uid)),
-		parquet.Int64Value(int64(sid)),
 		parquet.ByteArrayValue([]byte(prop.String())),
 		parquet.ByteArrayValue([]byte(metric.String())),
 		parquet.ByteArrayValue([]byte(key)),
@@ -61,25 +99,24 @@ func (v *V9) Add(ctx context.Context, ts int64, uid, sid uint64, prop spec.Prope
 	return nil
 }
 
-func (v *V9) save(ctx context.Context) error {
-	if len(v.rows) == 0 {
+func (v *V9) save(ctx context.Context, table *frostdb.Table, r *rowsBuffer) error {
+	if len(r.rows) == 0 {
 		return nil
 	}
-	_, err := v.buf.WriteRows(v.rows)
+	_, err := r.buf.WriteRows(r.rows)
 	if err != nil {
 		return err
 	}
-	_, err = v.table.InsertBuffer(ctx, v.buf)
+	_, err = table.InsertBuffer(ctx, r.buf)
 	if err != nil {
 		return err
 	}
-	v.buf.Reset()
-	v.rows = v.rows[:0]
+	r.reset()
 	return nil
 }
 
 func (v *V9) Close() error {
-	return errors.Join(v.save(context.Background()), v.db.Close(), v.store.Close())
+	return errors.Join(v.db.Close(), v.store.Close())
 }
 
 func Open(ctx context.Context, dataPath string) (*V9, error) {
@@ -98,81 +135,78 @@ func Open(ctx context.Context, dataPath string) (*V9, error) {
 		return nil, err
 	}
 
-	table, err := db.Table("vince", frostdb.NewTableConfig(scheme))
+	return &V9{store: store, db: db}, nil
+}
+
+var schema = must(dynparquet.SchemaFromDefinition(scheme))
+
+func must[T any](v T, err error) T {
 	if err != nil {
-		db.Close()
-		store.Close()
-		return nil, err
+		panic(err.Error())
 	}
-	buf, err := table.Schema().NewBuffer(nil)
-	if err != nil {
-		db.Close()
-		store.Close()
-		return nil, err
-	}
-	return &V9{store: store, db: db, table: table, buf: buf}, nil
+	return v
 }
 
 var scheme = &schemapb.Schema{
-	Name: "vince",
-	Columns: []*schemapb.Column{
-		{
-			Name: "timestamp",
-			StorageLayout: &schemapb.StorageLayout{
-				Type: schemapb.StorageLayout_TYPE_INT64,
+	Root: &schemapb.Group{
+		Name: "site_stats",
+		Nodes: []*schemapb.Node{
+			{
+				Type: &schemapb.Node_Leaf{
+					Leaf: &schemapb.Leaf{Name: "timestamp", StorageLayout: &schemapb.StorageLayout{
+						Type:        schemapb.StorageLayout_TYPE_INT64,
+						Compression: schemapb.StorageLayout_COMPRESSION_ZSTD,
+					}},
+				},
 			},
-		},
-		{
-			Name: "uid",
-			StorageLayout: &schemapb.StorageLayout{
-				Type: schemapb.StorageLayout_TYPE_INT64,
+			{
+				Type: &schemapb.Node_Leaf{
+					Leaf: &schemapb.Leaf{Name: "segment", StorageLayout: &schemapb.StorageLayout{
+						Type:        schemapb.StorageLayout_TYPE_STRING,
+						Encoding:    schemapb.StorageLayout_ENCODING_RLE_DICTIONARY,
+						Compression: schemapb.StorageLayout_COMPRESSION_ZSTD,
+					}},
+				},
 			},
-		},
-		{
-			Name: "sid",
-			StorageLayout: &schemapb.StorageLayout{
-				Type: schemapb.StorageLayout_TYPE_INT64,
+			{
+				Type: &schemapb.Node_Leaf{
+					Leaf: &schemapb.Leaf{Name: "metric", StorageLayout: &schemapb.StorageLayout{
+						Type:        schemapb.StorageLayout_TYPE_STRING,
+						Encoding:    schemapb.StorageLayout_ENCODING_RLE_DICTIONARY,
+						Compression: schemapb.StorageLayout_COMPRESSION_ZSTD,
+					}},
+				},
 			},
-		},
-		{
-			Name: "property",
-			StorageLayout: &schemapb.StorageLayout{
-				Type:     schemapb.StorageLayout_TYPE_STRING,
-				Encoding: schemapb.StorageLayout_ENCODING_RLE_DICTIONARY,
+			{
+				Type: &schemapb.Node_Leaf{
+					Leaf: &schemapb.Leaf{Name: "key", StorageLayout: &schemapb.StorageLayout{
+						Type:        schemapb.StorageLayout_TYPE_STRING,
+						Encoding:    schemapb.StorageLayout_ENCODING_RLE_DICTIONARY,
+						Compression: schemapb.StorageLayout_COMPRESSION_ZSTD,
+					}},
+				},
 			},
-		},
-		{
-			Name: "metric",
-			StorageLayout: &schemapb.StorageLayout{
-				Type:     schemapb.StorageLayout_TYPE_STRING,
-				Encoding: schemapb.StorageLayout_ENCODING_RLE_DICTIONARY,
-			},
-		},
-		{
-			Name: "key",
-			StorageLayout: &schemapb.StorageLayout{
-				Type:     schemapb.StorageLayout_TYPE_STRING,
-				Encoding: schemapb.StorageLayout_ENCODING_RLE_DICTIONARY,
-			},
-		},
-		{
-			Name: "value",
-			StorageLayout: &schemapb.StorageLayout{
-				Type: schemapb.StorageLayout_TYPE_INT64,
+			{
+				Type: &schemapb.Node_Leaf{
+					Leaf: &schemapb.Leaf{Name: "value", StorageLayout: &schemapb.StorageLayout{
+						Type:        schemapb.StorageLayout_TYPE_INT64,
+						Compression: schemapb.StorageLayout_COMPRESSION_ZSTD,
+					}},
+				},
 			},
 		},
 	},
 	SortingColumns: []*schemapb.SortingColumn{
 		{
-			Name:      "timestamp",
+			Path:      "timestamp",
 			Direction: schemapb.SortingColumn_DIRECTION_ASCENDING,
 		},
 		{
-			Name:      "uid",
+			Path:      "uid",
 			Direction: schemapb.SortingColumn_DIRECTION_ASCENDING,
 		},
 		{
-			Name:      "sid",
+			Path:      "sid",
 			Direction: schemapb.SortingColumn_DIRECTION_ASCENDING,
 		},
 	},
