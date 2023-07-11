36cf06cb0605186636b1ff39c46280e9e7014ff7
gernest <geofreyernest@live.com>
2023-07-11 15:18:47 +0300

    define sys schema

---
+++ internal/neo/sys.go
@@ -0,0 +1,16 @@
+package neo
+
+import (
+	"time"
+
+	"github.com/segmentio/parquet-go"
+)
+
+type Sys struct {
+	Timestamp time.Time         `parquet:"timestamp,timestamp,zstd"`
+	Labels    map[string]string `parquet-key:",dict,zstd" parquet-value:",dict,zstd"`
+	Name      string            `parquet:"name,zstd"`
+	Value     float64           `parquet:"value,zstd"`
+}
+
+var sysSchema = parquet.SchemaOf(Sys{})
