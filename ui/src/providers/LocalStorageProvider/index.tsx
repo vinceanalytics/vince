421eddc7282e00117ac40c5527640e9f0e8e7657
gernest <geofreyernest@live.com>
2023-08-03 17:53:48 +0300

    ui: bring back splitter basis

--- ui/src/providers/LocalStorageProvider/index.tsx
+++ ui/src/providers/LocalStorageProvider/index.tsx
@@ -6,7 +6,11 @@ import React, {
 } from "react"
 import {
     SettingsType,
-    StoreKey, getValue, setValue,
+    LocalConfig,
+    StoreKey,
+    getValue,
+    setValue,
+    parseInteger,
 } from './types'
 
 type Props = {}
@@ -14,11 +18,20 @@ type Props = {}
 
 type ContextProps = {
     authPayload: string
+    editorSplitterBasis: number
+    resultsSplitterBasis: number
     updateSettings: (key: StoreKey, value: SettingsType) => void
+}
+const defaultConfig: LocalConfig = {
+    authPayload: "",
+    editorSplitterBasis: 350,
+    resultsSplitterBasis: 350,
 }
 
 const defaultValues: ContextProps = {
     authPayload: "",
+    editorSplitterBasis: 350,
+    resultsSplitterBasis: 350,
     updateSettings: (key: StoreKey, value: SettingsType) => undefined,
 }
 
@@ -36,6 +49,18 @@ export const LocalStorageProvider = ({
         setValue(key, value.toString())
         refreshSettings(key)
     }
+    const [editorSplitterBasis, seteditorSplitterBasis] = useState<number>(
+        parseInteger(
+            getValue(StoreKey.EDITOR_SPLITTER_BASIS),
+            defaultConfig.editorSplitterBasis,
+        ),
+    )
+    const [resultsSplitterBasis, setresultsSplitterBasis] = useState<number>(
+        parseInteger(
+            getValue(StoreKey.RESULTS_SPLITTER_BASIS),
+            defaultConfig.resultsSplitterBasis,
+        ),
+    )
 
     const refreshSettings = (key: StoreKey) => {
         const value = getValue(key)
@@ -43,12 +68,24 @@ export const LocalStorageProvider = ({
             case StoreKey.AUTH_PAYLOAD:
                 setAuthPayload(value)
                 break
+            case StoreKey.EDITOR_SPLITTER_BASIS:
+                seteditorSplitterBasis(
+                    parseInteger(value, defaultConfig.editorSplitterBasis),
+                )
+                break
+            case StoreKey.RESULTS_SPLITTER_BASIS:
+                setresultsSplitterBasis(
+                    parseInteger(value, defaultConfig.resultsSplitterBasis),
+                )
+                break
         }
     }
     return (
         <LocalStorageContext.Provider
             value={{
                 authPayload,
+                editorSplitterBasis,
+                resultsSplitterBasis,
                 updateSettings,
             }}
         >
--- ui/src/providers/LocalStorageProvider/types.ts
+++ ui/src/providers/LocalStorageProvider/types.ts
@@ -1,5 +1,12 @@
 export enum StoreKey {
     AUTH_PAYLOAD = "AUTH_PAYLOAD",
+    QUERY_TEXT = "query.text",
+    EDITOR_LINE = "editor.line",
+    EDITOR_COL = "editor.col",
+    NOTIFICATION_ENABLED = "notification.enabled",
+    NOTIFICATION_DELAY = "notification.delay",
+    EDITOR_SPLITTER_BASIS = "splitter.editor.basis",
+    RESULTS_SPLITTER_BASIS = "splitter.results.basis",
 }
 
 export const getValue = (key: StoreKey) => localStorage.getItem(key) ?? ""
@@ -12,4 +19,12 @@ export type SettingsType = string | boolean | number
 
 export type LocalConfig = {
     authPayload: string
+    editorSplitterBasis: number
+    resultsSplitterBasis: number
 }
+
+export const parseBoolean = (value: string, defaultValue: boolean): boolean =>
+    value ? value === "true" : defaultValue
+
+export const parseInteger = (value: string, defaultValue: number): number =>
+    isNaN(parseInt(value)) ? defaultValue : parseInt(value)
