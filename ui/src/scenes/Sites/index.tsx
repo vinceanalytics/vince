0b72ec6bbc9674a20856943cc152317fa1b0ef55
gernest <geofreyernest@live.com>
2023-08-06 13:01:11 +0300

    ui: another take on action menu

--- ui/src/scenes/Sites/index.tsx
+++ ui/src/scenes/Sites/index.tsx
@@ -3,52 +3,13 @@ import { TableIcon, PlusIcon, GoalIcon, AlertIcon } from "@primer/octicons-react
 
 import { UnderlineNav } from '@primer/react/drafts'
 import SitePanel from "./sitePanel";
-import GoalsPanel from "./goalsPanel";
-import AlertsPanel from "./alertsPanel";
 import { useState } from "react";
 
 
 
 const Sites = () => {
-    const [active, setActive] = useState<string>("sites")
     return (
-        <Box
-            sx={{
-                display: "flex",
-                overflow: "auto",
-                flex: "1",
-                flexDirection: "column",
-                paddingLeft: "2px",
-            }}>
-            <UnderlineNav aria-label="Sites" >
-                <UnderlineNav.Item
-                    aria-current={active === "sites" ? true : undefined}
-                    onSelect={() => setActive("sites")}
-                    icon={TableIcon}
-                >
-                    Sites
-                </UnderlineNav.Item>
-                <UnderlineNav.Item
-                    aria-current={active === "goals" ? true : undefined}
-                    onSelect={() => setActive("goals")}
-                    icon={GoalIcon}
-                >
-                    Goals
-                </UnderlineNav.Item>
-                <UnderlineNav.Item
-                    aria-current={active === "alerts" ? true : undefined}
-                    onSelect={() => setActive("alerts")}
-                    icon={AlertIcon}
-                >
-                    Alerts
-                </UnderlineNav.Item>
-            </UnderlineNav>
-            <Box>
-                {active === "sites" && <SitePanel />}
-                {active === "goals" && <GoalsPanel />}
-                {active === "alerts" && <AlertsPanel />}
-            </Box>
-        </Box>
+        <SitePanel />
     )
 }
 
--- ui/src/scenes/Sites/sitePanel.tsx
+++ ui/src/scenes/Sites/sitePanel.tsx
@@ -1,48 +1,88 @@
-import { useState, useCallback } from "react";
-import { Text, TreeView, Box, Button, IconButton, Link } from "@primer/react";
-import { PlusIcon } from "@primer/octicons-react";
+import { useState, useCallback, ReactNode } from "react";
+import { Text, TreeView, Box, themeGet, IconButton, Octicon } from "@primer/react";
+import { PlusIcon, DatabaseIcon } from "@primer/octicons-react";
 import { Dialog } from '@primer/react/drafts'
 
+import styled, { css } from "styled-components"
+
+export const PaneMenu = styled.div`
+  position: relative;
+  display: flex;
+  height: 3rem;
+  padding: 0 1rem;
+  align-items: center;
+  background: ${themeGet("canvas.default")};
+  border-top: 1px solid transparent;
+  z-index: 5;
+`
+
+const loadingStyles = css`
+  display: flex;
+  justify-content: center;
+`
+
+export const PaneWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+`
+
+const Wrapper = styled(PaneWrapper)`
+  overflow-x: auto;
+  height: 100%;
+`
+
+const Menu = styled(PaneMenu)`
+  justify-content: space-between;
+`
+
+
+const Header = styled(Text)`
+  display: flex;
+  align-items: center;
+`
+
+type Props = Readonly<{
+    children: ReactNode
+    className?: string
+}>
+
+export const PaneContent = styled.div<Props>`
+    display: flex;
+    flex-direction: column;
+    flex: 1;
+    background: ${themeGet("canvas.default")};
+    overflow: auto;
+  `
+
+const Content = styled(PaneContent) <{
+    _loading: boolean
+}>`
+    display: block;
+    overflow: auto;
+    ${({ _loading }) => _loading && loadingStyles};
+  `
+
+
+const DBIcon = styled(DatabaseIcon)`
+margin-right: 1rem;
+`
 
 const SitePanel = () => {
-    const [isOpen, setIsOpen] = useState<boolean>(false)
-    const openDialog = useCallback(() => setIsOpen(true), [setIsOpen])
-    const closeDialog = useCallback(() => setIsOpen(false), [setIsOpen])
     return (
-        <nav aria-label="Sites Content">
-            <TreeView aria-label="Sites Content">
-                <TreeView.Item id="root">
-                    <TreeView.LeadingVisual>
-                        <TreeView.DirectoryIcon />
-                    </TreeView.LeadingVisual>
-                    ..
-                    <TreeView.SubTree>
-                        <TreeView.Item id="createSite" >
-                            <TreeView.LeadingVisual>
-                                <PlusIcon />
-                            </TreeView.LeadingVisual>
-                            <Link onClick={openDialog}>
-                                Create Site
-                            </Link>
-                            {isOpen && (
-                                <Dialog
-                                    title="Dialog example"
-                                    subtitle={
-                                        <>
-                                            This is a <b>description</b> of the dialog.
-                                        </>
-                                    }
-                                    footerButtons={[{ content: 'Ok', onClick: closeDialog }]}
-                                    onClose={closeDialog}
-                                >
-                                    <Text fontFamily="sans-serif">Some content</Text>
-                                </Dialog>
-                            )}
-                        </TreeView.Item>
-                    </TreeView.SubTree>
-                </TreeView.Item>
-            </TreeView>
-        </nav>
+        <Wrapper>
+            <Box sx={{ borderBottomWidth: 1, borderBottomStyle: 'solid', borderColor: 'border.default' }}>
+                < Menu >
+                    <Header>
+                        <Octicon icon={DatabaseIcon} sx={{ marginRight: "1rem" }} />
+                        Sites
+                    </Header>
+                    <Box display={"flex"}>
+                        <IconButton size="small" aria-label="new site" icon={PlusIcon} />
+                    </Box>
+                </Menu>
+            </Box >
+        </Wrapper >
     )
 }
 
