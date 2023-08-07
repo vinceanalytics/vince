fbe389890b9c12b6f3874a828c34509da453b015
gernest <geofreyernest@live.com>
2023-08-07 11:40:04 +0300

    ui: add form validation

--- ui/src/scenes/Sites/index.tsx
+++ ui/src/scenes/Sites/index.tsx
@@ -1,7 +1,7 @@
-import { useState, useCallback, ReactNode } from "react";
-import { Text, TreeView, Box, themeGet, IconButton, Octicon } from "@primer/react";
+import { useState, useCallback, ReactNode, useEffect } from "react";
+import { Text, TextInput, FormControl, TreeView, Box, themeGet, IconButton, Octicon } from "@primer/react";
 import { PlusIcon, DatabaseIcon } from "@primer/octicons-react";
-import { Dialog } from '@primer/react/drafts'
+import { Dialog, PageHeader } from '@primer/react/drafts'
 
 import styled, { css } from "styled-components"
 import { Site } from "../../vince";
@@ -66,40 +66,73 @@ const Content = styled(PaneContent) <{
 
 
 
-type NewSite = {
-  isDialogOpen: boolean;
-  site?: Site;
-}
 
+const domainRe = new RegExp("^(?!-)[A-Za-z0-9-]+([-.]{1}[a-z0-9]+)*.[A-Za-z]{2,6}$")
 
 const Sites = () => {
   const [isOpen, setIsOpen] = useState(false);
   const openDialog = useCallback(() => setIsOpen(true), [setIsOpen])
   const closeDialog = useCallback(() => setIsOpen(false), [setIsOpen])
+  const [domain, setDomain] = useState<string>("")
 
+  const submitNewSite = useCallback(() => {
+    console.log(domain)
+    setIsOpen(false)
+  }, [domain])
+  const [validDomain, setValidDomain] = useState(true)
+
+  useEffect(() => {
+    if (domain != "") {
+      if (domainRe.test(domain)) {
+        setValidDomain(true)
+      } else {
+        setValidDomain(false)
+      }
+    }
+  }, [domain])
 
   return (
     <Wrapper>
-      <Box sx={{ borderBottomWidth: 1, borderBottomStyle: 'solid', borderColor: 'border.default' }}>
-        < Menu >
-          <Header>
-            <Octicon icon={DatabaseIcon} sx={{ marginRight: "1rem" }} />
-            Sites
-          </Header>
-          <Box display={"flex"}>
-            <IconButton size="small" aria-label="new site" icon={PlusIcon} onClick={openDialog} />
-            {isOpen && (
-              <Dialog
-                title="Create New Site"
-                footerButtons={[{ content: 'Create', onClick: closeDialog }]}
-                onClose={closeDialog}
-              >
-                <Text fontFamily="sans-serif">Some content</Text>
-              </Dialog>
-            )}
-          </Box>
-        </Menu>
-      </Box >
+      <Box paddingX={2}>
+        <PageHeader>
+          <PageHeader.TitleArea>
+            <PageHeader.LeadingAction>
+              <DatabaseIcon />
+            </PageHeader.LeadingAction>
+            <PageHeader.Title>Sites</PageHeader.Title>
+            <PageHeader.Actions>
+              <IconButton variant="primary" aria-label="New Site" icon={PlusIcon} onClick={openDialog} />
+              {isOpen && (
+                <Dialog
+                  title="Create New Site"
+                  footerButtons={
+                    [{
+                      content: 'Create', onClick: submitNewSite,
+                    }]
+                  }
+                  onClose={closeDialog}
+                >
+                  <Box>
+                    <FormControl>
+                      <FormControl.Label>Domain</FormControl.Label>
+                      <TextInput
+                        monospace
+                        block
+                        placeholder="vinceanalytics.github.io"
+                        onChange={(e) => setDomain(e.currentTarget.value)}
+                      />
+                      {!validDomain &&
+                        <FormControl.Validation id="new-site" variant="error">
+                          Domain must be the
+                        </FormControl.Validation>}
+                    </FormControl>
+                  </Box>
+                </Dialog>
+              )}
+            </PageHeader.Actions>
+          </PageHeader.TitleArea>
+        </PageHeader>
+      </Box>
     </Wrapper >
   )
 }
