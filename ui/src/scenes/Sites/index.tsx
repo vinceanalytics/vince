ca41dcf6a45560eb08621cb1d061dc7fda704f83
gernest <geofreyernest@live.com>
2023-08-07 16:12:32 +0300

    ui: cleanup sites component

--- ui/src/scenes/Sites/index.tsx
+++ ui/src/scenes/Sites/index.tsx
@@ -9,21 +9,6 @@ import { Dialog, PageHeader } from '@primer/react/drafts'
 import styled, { css } from "styled-components"
 import { Site, Client } from "../../vince";
 
-export const PaneMenu = styled.div`
-  position: relative;
-  display: flex;
-  height: 3rem;
-  padding: 0 1rem;
-  align-items: center;
-  background: ${themeGet("canvas.default")};
-  border-top: 1px solid transparent;
-  z-index: 5;
-`
-
-const loadingStyles = css`
-  display: flex;
-  justify-content: center;
-`
 
 export const PaneWrapper = styled.div`
   display: flex;
@@ -36,36 +21,9 @@ const Wrapper = styled(PaneWrapper)`
   height: 100%;
 `
 
-const Menu = styled(PaneMenu)`
-  justify-content: space-between;
-`
 
 
-const Header = styled(Text)`
-  display: flex;
-  align-items: center;
-`
 
-type Props = Readonly<{
-  children: ReactNode
-  className?: string
-}>
-
-export const PaneContent = styled.div<Props>`
-    display: flex;
-    flex-direction: column;
-    flex: 1;
-    background: ${themeGet("canvas.default")};
-    overflow: auto;
-  `
-
-const Content = styled(PaneContent) <{
-  _loading: boolean
-}>`
-    display: block;
-    overflow: auto;
-    ${({ _loading }) => _loading && loadingStyles};
-  `
 
 
 
