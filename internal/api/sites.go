446ca37ef8f638d4e8a8cdde561043578038d2d5
gernest <geofreyernest@live.com>
2023-08-08 08:19:31 +0300

    implement list sites from badger storage

--- internal/api/sites.go
+++ internal/api/sites.go
@@ -3,10 +3,30 @@ package api
 import (
 	"net/http"
 
+	"github.com/dgraph-io/badger/v4"
+	"github.com/vinceanalytics/vince/internal/must"
 	"github.com/vinceanalytics/vince/internal/render"
+	"github.com/vinceanalytics/vince/internal/timeseries"
 	v1 "github.com/vinceanalytics/vince/proto/v1"
+	"google.golang.org/protobuf/proto"
 )
 
 func ListSites(w http.ResponseWriter, r *http.Request) {
-	render.JSON(w, http.StatusOK, []v1.Site{})
+	o := []*v1.Site{}
+	timeseries.Store(r.Context()).View(func(txn *badger.Txn) error {
+		itO := badger.DefaultIteratorOptions
+		prefix := []byte(v1.StorePrefix_SITES.String())
+		itO.Prefix = prefix
+		it := txn.NewIterator(itO)
+		for it.Rewind(); it.Valid(); it.Next() {
+			it.Item().Value(func(val []byte) error {
+				var n v1.Site
+				must.One(proto.Unmarshal(val, &n))()
+				o = append(o, &n)
+				return nil
+			})
+		}
+		return nil
+	})
+	render.JSON(w, http.StatusOK, o)
 }
