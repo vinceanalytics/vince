bf5821cf7b680990e5020dedff5d15ab6eee1f3f
gernest <geofreyernest@live.com>
2023-08-10 10:20:44 +0300

    add simple_select_where

--- internal/be/sql.go
+++ internal/be/sql.go
@@ -3,10 +3,14 @@ package be
 import (
 	"fmt"
 
+	"github.com/apache/arrow/go/v13/arrow"
 	"github.com/pingcap/tidb/parser"
 	"github.com/pingcap/tidb/parser/ast"
-	_ "github.com/pingcap/tidb/parser/test_driver"
+	"github.com/pingcap/tidb/parser/opcode"
+	"github.com/pingcap/tidb/parser/test_driver"
+	"github.com/substrait-io/substrait-go/expr"
 	"github.com/substrait-io/substrait-go/plan"
+	"github.com/vinceanalytics/vince/internal/must"
 )
 
 type Parser struct {
@@ -49,8 +53,10 @@ type astVisitor struct {
 	explain bool
 	plan    plan.Builder
 	err     error
-	root    plan.Rel
+	rel     plan.Rel
 	names   []string
+	expr    []expr.Expression
+	xb      expr.ExprBuilder
 }
 
 var _ ast.Visitor = (*astVisitor)(nil)
@@ -66,21 +72,23 @@ func (f leaveFunc) Enter(n ast.Node) (ast.Node, bool) {
 func (f leaveFunc) Leave(n ast.Node) (ast.Node, bool) {
 	return f(n)
 }
+
 func newASTVisitor() *astVisitor {
 	return &astVisitor{
 		plan: plan.NewBuilderDefault(),
+		xb:   expr.ExprBuilder{Reg: extSet.GetSubstraitRegistry()},
 	}
 }
 
 func (v *astVisitor) Build() (*plan.Plan, error) {
-	return v.plan.Plan(v.root, v.names)
+	return v.plan.Plan(v.rel, v.names)
 }
 
 func (v *astVisitor) Enter(n ast.Node) (nRes ast.Node, skipChildren bool) {
-	switch expr := n.(type) {
+	switch e := n.(type) {
 	case *ast.SelectStmt:
 		var name *ast.TableName
-		expr.From.Accept(leaveFunc(func(n ast.Node) (node ast.Node, ok bool) {
+		e.From.Accept(leaveFunc(func(n ast.Node) (node ast.Node, ok bool) {
 			switch e := n.(type) {
 			case *ast.TableName:
 				name = e
@@ -89,7 +97,7 @@ func (v *astVisitor) Enter(n ast.Node) (nRes ast.Node, skipChildren bool) {
 			return n, true
 		}))
 		var columns []string
-		expr.Fields.Accept(leaveFunc(func(n ast.Node) (node ast.Node, ok bool) {
+		e.Fields.Accept(leaveFunc(func(n ast.Node) (node ast.Node, ok bool) {
 			switch e := n.(type) {
 			case *ast.SelectField:
 				if e.WildCard != nil {
@@ -100,15 +108,93 @@ func (v *astVisitor) Enter(n ast.Node) (nRes ast.Node, skipChildren bool) {
 				columns = append(columns, e.Name.String())
 				return n, true
 			default:
-				fmt.Printf("FROM %#T\n", e)
 			}
 			return n, true
 		}))
-
-		v.root = v.plan.NamedScan([]string{name.Name.String()},
-			Schema(columns...),
+		root := Schema(columns...)
+		table := v.plan.NamedScan([]string{name.Name.String()},
+			root,
 		)
+		v.xb.BaseSchema = &root.Struct
+		v.rel = table
 		v.names = append(v.names[:0], columns...)
+		if e.Where != nil {
+			e.Where.Accept(leaveFunc(func(n ast.Node) (node ast.Node, ok bool) {
+				switch e := n.(type) {
+				case *ast.ColumnName:
+					name := e.Name.String()
+					for i := range v.names {
+						if v.names[i] == name {
+							v.expr = append(v.expr,
+								must.Must(
+									v.plan.RootFieldRef(table, int32(i)),
+								)(),
+							)
+							return n, true
+						}
+					}
+					v.err = fmt.Errorf("column %q does not exist", name)
+					return nil, false
+				case *test_driver.ValueExpr:
+					var lit expr.Literal
+					switch x := e.GetValue().(type) {
+					case int64:
+						lit = expr.NewPrimitiveLiteral(x, false)
+					case float32:
+						lit = expr.NewPrimitiveLiteral(x, false)
+					case float64:
+						lit = expr.NewPrimitiveLiteral(x, false)
+					case string:
+						lit = expr.NewPrimitiveLiteral(x, false)
+					default:
+						v.err = fmt.Errorf("data type %v is not supported", x)
+						return nil, false
+					}
+					v.expr = append(v.expr, lit)
+				case *ast.BinaryOperationExpr:
+					right, n := pop(v.expr)
+					left, n := pop(n)
+					v.expr = n
+					var op string
+					switch e.Op {
+					case opcode.GT:
+						op = "greater"
+					case opcode.LT:
+						op = "less"
+					case opcode.GE:
+						op = "greater_equal"
+					case opcode.LE:
+						op = "less_equal"
+					case opcode.EQ:
+						op = "equal"
+					case opcode.NE:
+						op = "not_equal"
+						v.err = fmt.Errorf("operator %q is not supported", e.Op)
+						return nil, false
+					}
+					f, err := v.callScalar(op, left, right)
+					if err != nil {
+						v.err = err
+						return nil, false
+					}
+					v.expr = append(v.expr, f)
+				default:
+					fmt.Printf("WHERE %#T\n", e)
+				}
+				return n, true
+			}))
+			if v.err != nil {
+				return nil, true
+			}
+			cond, n := pop(v.expr)
+			v.expr = n
+			f, err := v.plan.Filter(table, cond)
+			if err != nil {
+				v.err = err
+				return nil, true
+			}
+			v.rel = f
+		}
 
 	// 	// The SelectStmt is handled in during pre-visit given that it has many
 	// 	// clauses we need to handle independently (e.g. a group by with a
@@ -142,7 +228,7 @@ func (v *astVisitor) Enter(n ast.Node) (nRes ast.Node, skipChildren bool) {
 	// 	}
 	// 	return n, true
 	default:
-		fmt.Printf("=> %#T\n", expr)
+		fmt.Printf("=> %#T\n", e)
 	}
 	return n, false
 }
@@ -293,4 +379,28 @@ func columnNameToString(c *ast.ColumnName) string {
 func pop[T any](s []T) (T, []T) {
 	lastIdx := len(s) - 1
 	return s[lastIdx], s[:lastIdx]
+}
+
+func (v *astVisitor) callScalar(name string, args ...expr.Expression) (*expr.ScalarFunction, error) {
+	conv, ok := extSet.GetArrowRegistry().GetArrowToSubstrait(name)
+	if !ok {
+		return nil, arrow.ErrNotFound
+	}
+	id, opts, err := conv(name)
+	if err != nil {
+		return nil, err
+	}
+
+	a := make([]expr.FuncArgBuilder, len(args))
+	for i := range args {
+		switch e := args[i].(type) {
+		case expr.Literal:
+			a[i] = v.xb.Literal(e)
+		case *expr.FieldReference:
+			a[i] = v.xb.RootRef(e.Reference)
+		default:
+			return nil, fmt.Errorf("unsupported argument type %#T", e)
+		}
+	}
+	return v.xb.ScalarFunc(id, opts...).Args(a...).Build()
 }
--- internal/be/sql_test.go
+++ internal/be/sql_test.go
@@ -40,3 +40,21 @@ func TestParse_simple_select_columns(t *testing.T) {
 		t.Error("plan mismatch")
 	}
 }
+
+func TestParse_simple_where(t *testing.T) {
+	p := NewParser()
+	r := must.Must(p.Parse("select * from `vince.io` where name='pageview'"))()
+	got := must.Must(
+		json.MarshalIndent(
+			must.Must(r.Plan.ToProto())(),
+			"", "  ",
+		),
+	)()
+	os.WriteFile("testdata/simple_select_where.json", got, 0600)
+	want := must.Must(os.ReadFile("testdata/simple_select_where.json"))()
+	if !bytes.Equal(got, want) {
+		t.Error("plan mismatch")
+	}
+	t.Error()
+
+}
