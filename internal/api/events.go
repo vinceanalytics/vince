ee2b2e88e4a5a7b04b9d9c9c69fe46c822f8eaf5
gernest <geofreyernest@live.com>
2023-08-11 12:44:14 +0300

    deny events from non registered sites

--- internal/api/events.go
+++ internal/api/events.go
@@ -1,14 +1,18 @@
 package api
 
 import (
+	"context"
 	"net/http"
 
+	"github.com/dgraph-io/badger/v4"
 	"github.com/vinceanalytics/vince/internal/core"
+	"github.com/vinceanalytics/vince/internal/db"
 	"github.com/vinceanalytics/vince/internal/events"
 	"github.com/vinceanalytics/vince/internal/remoteip"
 	"github.com/vinceanalytics/vince/internal/timeseries"
 	"github.com/vinceanalytics/vince/pkg/entry"
 	"github.com/vinceanalytics/vince/pkg/log"
+	v1 "github.com/vinceanalytics/vince/proto/v1"
 )
 
 // Events accepts events payloads from vince client script.
@@ -25,16 +29,32 @@ func Events(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-
 	req.IP = remoteip.Get(r)
 	req.UserAgent = r.UserAgent()
-
+	ctx := r.Context()
+	if !accept(ctx, req.Domain) {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
 	e, err := events.Parse(req, core.Now(r.Context()))
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
-	timeseries.Register(r.Context(), e)
+	timeseries.Register(ctx, e)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("ok"))
+}
+
+// returns true if domain is events are accepted. CHecks is done to only ensure
+// there is an existing site registered with the domain.
+func accept(ctx context.Context, domain string) bool {
+	return db.Get(ctx).View(func(txn *badger.Txn) error {
+		key := (&v1.StoreKey{
+			Prefix: v1.StorePrefix_SITES,
+			Key:    domain,
+		}).Badger()
+		_, err := txn.Get([]byte(key))
+		return err
+	}) == nil
 }
