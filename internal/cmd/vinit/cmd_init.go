654fad2658882cc5239de908180d0cd503b11c91
gernest <geofreyernest@live.com>
2023-08-15 10:48:25 +0300

    support passing working directory

--- internal/cmd/vinit/cmd_init.go
+++ internal/cmd/vinit/cmd_init.go
@@ -2,6 +2,7 @@ package vinit
 
 import (
 	"os"
+	"path/filepath"
 
 	"github.com/urfave/cli/v3"
 	"github.com/vinceanalytics/vince/internal/config"
@@ -14,14 +15,32 @@ func CMD() *cli.Command {
 		Name:  "init",
 		Usage: "Initializes a vince project",
 		Action: func(ctx *cli.Context) error {
-			must.One(os.Mkdir(config.META_PATH, 0755))(
+			root := ctx.Args().First()
+			if root != "" {
+				// Try to make sure the directory exists. No need to check for error here
+				// because we bail out first thing when we cant write to this path.
+				os.MkdirAll(root, 0755)
+			}
+			meta := config.META_PATH
+			if root != "" {
+				meta = filepath.Join(root, meta)
+			}
+			must.One(os.Mkdir(meta, 0755))(
 				"failed to create metadata directory",
 			)
-			must.One(os.Mkdir(config.BLOCKS_PATH, 0755))(
+			blocks := config.BLOCKS_PATH
+			if root != "" {
+				blocks = filepath.Join(root, blocks)
+			}
+			must.One(os.Mkdir(blocks, 0755))(
 				"failed to create blocks directory",
 			)
 			b := must.Must(pj.MarshalIndent(config.Defaults()))()
-			must.One(os.WriteFile(config.FILE, b, 0600))(
+			file := config.FILE
+			if root != "" {
+				file = filepath.Join(root, file)
+			}
+			must.One(os.WriteFile(file, b, 0600))(
 				"failed to create vince configuration",
 			)
 			return nil
--- internal/config/config.go
+++ internal/config/config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"context"
 	"os"
+	"path/filepath"
 
 	"github.com/urfave/cli/v3"
 	"github.com/vinceanalytics/vince/internal/must"
@@ -18,13 +19,45 @@ func Get(ctx context.Context) *Options {
 }
 
 func Load(base *Options, x *cli.Context) (context.Context, error) {
+	root := x.Args().First()
 	base.SyncInterval = durationpb.New(x.Duration("sync-interval"))
-	b := must.Must(os.ReadFile(FILE))(
+	file := FILE
+	if root != "" {
+		file = filepath.Join(root, file)
+	}
+	b := must.Must(os.ReadFile(file))(
 		"called vince on non vince project, call vince init and try again",
 	)
 	var f Options
 	must.One(pj.Unmarshal(b, &f))("invalid configuration file")
 	proto.Merge(base, &f)
+
+	// resolve paths to their absolute values when file path is passed as first
+	// argument to serve.
+	if root := x.Args().First(); root != "" {
+		if !filepath.IsAbs(root) {
+			root = must.Must(filepath.Abs(root))(
+				"failed resolving absolute path",
+			)
+		}
+		if !filepath.IsAbs(base.MetaPath) {
+			base.MetaPath = filepath.Join(root, base.MetaPath)
+		}
+		if !filepath.IsAbs(base.BlocksPath) {
+			base.BlocksPath = filepath.Join(root, base.BlocksPath)
+		}
+	} else {
+		if !filepath.IsAbs(base.MetaPath) {
+			base.MetaPath = must.Must(filepath.Abs(base.MetaPath))(
+				"failed to to resolve absolute path for meta",
+			)
+		}
+		if !filepath.IsAbs(base.BlocksPath) {
+			base.BlocksPath = must.Must(filepath.Abs(base.BlocksPath))(
+				"failed to to resolve absolute path for blocks",
+			)
+		}
+	}
 	baseCtx := context.WithValue(context.Background(), configKey{}, base)
 	return baseCtx, nil
 }
