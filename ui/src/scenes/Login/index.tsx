00710c2a73a1d4bb45b3787a81beec9227b514a5
gernest <geofreyernest@live.com>
2023-08-21 18:51:31 +0300

    chore: update login form
    Autocomplete username and password

--- ui/src/scenes/Login/index.tsx
+++ ui/src/scenes/Login/index.tsx
@@ -1,6 +1,6 @@
 
 import { Box, Button, TextInput } from "@primer/react";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import { Client, TokenResult } from "../../vince";
 import { useLocalStorage, StoreKey } from "../../providers/LocalStorageProvider";
 
@@ -11,14 +11,15 @@ export const Login = () => {
     const [password, setPassWord] = useState<string>("")
     const [loading, setLoading] = useState<boolean>(false)
     const submit = useCallback(() => {
+        setLoading(true)
         vince.login({
             name: userName,
             password: password,
             generate: true,
         }).then((result) => {
+            setLoading(false);
             const r = result as TokenResult;
             updateSettings(StoreKey.AUTH_PAYLOAD, r.token)
-            setLoading(false);
         })
             .catch((e) => {
                 console.log(e)
@@ -32,28 +33,32 @@ export const Login = () => {
             justifyContent={"center"}
             alignItems={"center"}
         >
-            <Box display={"flex"}>
-                <Box display={"flex"} flex={1} alignItems={"center"} justifyContent={"center"} mr={1}>
-                    <a href="https://vinceanalytics.github.io" rel="noreferrer" target="_blank">
-                        <img alt="VinceAnalytics Logo" height="26" src="/logo.svg" />
-                    </a>
+            <form>
+                <Box display={"flex"}>
+                    <Box display={"flex"} flex={1} alignItems={"center"} justifyContent={"center"} mr={1}>
+                        <a href="https://vinceanalytics.github.io" rel="noreferrer" target="_blank">
+                            <img alt="VinceAnalytics Logo" height="26" src="/logo.svg" />
+                        </a>
+                    </Box>
+                    <TextInput
+                        name="username"
+                        aria-label="vince root account name"
+                        required
+                        onChange={(e) => setUserName(e.currentTarget.value)}
+                        loading={loading ? true : undefined}
+                        autoComplete="username"
+                        monospace placeholder="username" sx={{ marginRight: 1 }} />
+                    <TextInput
+                        name="password"
+                        aria-label="vince root account password"
+                        required
+                        onChange={(e) => setPassWord(e.currentTarget.value)}
+                        loading={loading ? true : undefined}
+                        autoComplete="current-password"
+                        monospace placeholder="password" type="password" sx={{ marginRight: 1 }} />
+                    <Button variant="primary" onClick={submit}>Login</Button>
                 </Box>
-                <TextInput
-                    name="username"
-                    aria-label="vince root account name"
-                    required
-                    onChange={(e) => setUserName(e.currentTarget.value)}
-                    loading={loading}
-                    monospace placeholder="username" sx={{ marginRight: 1 }} />
-                <TextInput
-                    name="password"
-                    aria-label="vince root account password"
-                    required
-                    onChange={(e) => setPassWord(e.currentTarget.value)}
-                    loading={loading}
-                    monospace placeholder="password" type="password" sx={{ marginRight: 1 }} />
-                <Button variant="primary" onClick={submit}>Login</Button>
-            </Box>
+            </form>
         </Box>
     )
 }
