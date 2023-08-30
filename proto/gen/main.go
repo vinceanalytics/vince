1ea5d5f65e02e85974c6eefc74827814c7e45e71
gernest <geofreyernest@live.com>
2023-08-30 18:39:05 +0300

    remove bagder proto
    We only replicate at entry level.

--- proto/gen/main.go
+++ proto/gen/main.go
@@ -9,10 +9,4 @@ func main() {
 		"-I=.", "--go_out=paths=source_relative:.",
 		"api.proto",
 	)
-	tools.ExecPlainWithWorkingPath(
-		"./bpb",
-		"protoc",
-		"-I=.", "--go_out=paths=source_relative:.",
-		"bpb.proto",
-	)
 }
