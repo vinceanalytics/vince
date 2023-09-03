ace7108bb8f2cca8d699bac2cdb720124f57a139
gernest <geofreyernest@live.com>
2023-09-04 01:55:46 +0300

    chore: remove unused code

--- internal/tools/tools.go
+++ internal/tools/tools.go
@@ -1,22 +1,15 @@
 package tools
 
 import (
-	"bufio"
 	"bytes"
-	"encoding/json"
-	"fmt"
-	"io"
 	"log"
 	"os"
 	"os/exec"
 	"path/filepath"
 	"runtime/debug"
-	"sort"
 	"strconv"
 	"strings"
 	"sync"
-	"text/tabwriter"
-	"time"
 
 	"golang.org/x/mod/semver"
 )
@@ -114,115 +107,6 @@ func CopyDir(src, dest string, workingDir ...string) {
 func Exit(a ...string) {
 	println(">>> ", strings.Join(a, " "))
 	os.Exit(1)
-}
-
-// Table generates nice looking markdown tables
-type Table struct {
-	bytes.Buffer
-	b    bytes.Buffer
-	line bytes.Buffer
-	tw   tabwriter.Writer
-}
-
-func (t *Table) Init(header ...string) *Table {
-	t.b.Reset()
-	t.tw.Init(&t.b, 0, 0, 1, ' ', tabwriter.AlignRight|tabwriter.Debug)
-	dash := make([]string, len(header))
-	for i := range dash {
-		dash[i] = "----"
-	}
-	t.Row(header...)
-	t.Row(dash...)
-	return t
-}
-
-func (t *Table) Row(entries ...string) {
-	t.line.Reset()
-	for i := range entries {
-		if i != 0 {
-			t.line.WriteRune('\t')
-		}
-		t.line.WriteString(entries[i])
-	}
-	t.line.WriteByte('\n')
-	io.Copy(&t.tw, &t.line)
-}
-
-func (t *Table) Flush() {
-	t.tw.Flush()
-	t.Buffer.Reset()
-	s := bufio.NewScanner(&t.b)
-	for s.Scan() {
-		fmt.Fprintf(&t.Buffer, "|%s|\n", s.Text())
-	}
-}
-
-type Artifact struct {
-	Name  string `json:"name"`
-	Type  string `json:"type"`
-	Path  string `json:"path"`
-	Os    string `json:"goos"`
-	Arch  string `json:"goarch"`
-	Extra struct {
-		ID           string `json:"id"`
-		Size         int
-		DockerConfig struct {
-			ID string `json:"id"`
-		} `json:"DockerConfig"`
-	} `json:"extra"`
-}
-
-type MetaData struct {
-	Name    string    `json:"project_name"`
-	Tag     string    `json:"tag"`
-	Version string    `json:"version"`
-	Commit  string    `json:"commit"`
-	Date    time.Time `json:"date"`
-}
-
-type Project struct {
-	Meta      MetaData
-	Artifacts []*Artifacts
-}
-
-type Artifacts struct {
-	ID        string
-	Artifacts []Artifact
-}
-
-func Release(root string) (p Project) {
-	readJSON(filepath.Join(root, "dist/metadata.json"), &p.Meta)
-	var artifacts []Artifact
-	readJSON(filepath.Join(root, "dist/artifacts.json"), &artifacts)
-	m := make(map[string][]Artifact)
-	for _, a := range artifacts {
-		switch a.Type {
-		case "Archive", "Linux Package":
-			m[a.Extra.ID] = append(m[a.Extra.ID], a)
-		}
-	}
-	for k, v := range m {
-		p.Artifacts = append(p.Artifacts, &Artifacts{
-			ID:        k,
-			Artifacts: v,
-		})
-	}
-	sort.Slice(p.Artifacts, func(i, j int) bool {
-		// This is to ensure vince comes before v8s
-		return p.Artifacts[i].ID > p.Artifacts[j].ID
-	})
-	return
-}
-
-func readJSON(path string, o any) {
-	b, err := os.ReadFile(path)
-	if err != nil {
-		Exit("failed to read", path, err.Error())
-	}
-	err = json.Unmarshal(b, o)
-	if err != nil {
-		Exit("failed to decode", path, err.Error())
-	}
 }
 
 func Package(module string) *debug.Module {
