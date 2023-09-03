83e2a055424186b1cea0e8ac85933343eb8d8d7f
gernest <geofreyernest@live.com>
2023-09-03 20:41:02 +0300

    fix events metrics

--- internal/api/events.go
+++ internal/api/events.go
@@ -2,58 +2,42 @@ package api
 
 import (
 	"context"
+	"log/slog"
 	"net/http"
 
-	"log/slog"
-
-	"github.com/prometheus/client_golang/prometheus"
 	apiv1 "github.com/vinceanalytics/vince/gen/proto/go/vince/api/v1"
 	"github.com/vinceanalytics/vince/internal/db"
 	"github.com/vinceanalytics/vince/internal/entry"
 	"github.com/vinceanalytics/vince/internal/keys"
+	"github.com/vinceanalytics/vince/internal/metrics"
 	"github.com/vinceanalytics/vince/internal/pj"
 	"github.com/vinceanalytics/vince/internal/remoteip"
 	"github.com/vinceanalytics/vince/internal/worker"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
-var AcceptedEvents = prometheus.NewCounter(prometheus.CounterOpts{
-	Namespace: "vince",
-	Name:      "events_accepted_total",
-	Help:      "Total number of analytics events accepted",
-})
-
-var RejectedEvents = prometheus.NewCounter(prometheus.CounterOpts{
-	Namespace: "vince",
-	Name:      "events_rejected_total",
-	Help:      "Total number of analytics events rejected",
-})
-
-func init() {
-	prometheus.MustRegister(AcceptedEvents, RejectedEvents)
-}
-
 // Events accepts events payloads from vince client script.
 func Events(w http.ResponseWriter, r *http.Request) {
+	ctx := r.Context()
 	w.Header().Set("X-Content-Type-Options", "nosniff")
-	xlg := slog.Default().WithGroup("api").
-		With("path", r.URL.Path, "method", r.Method)
-
 	req := &entry.Request{}
-
 	err := pj.UnmarshalDefault(req, r.Body)
 	if err != nil {
-		xlg.Error("failed parsing request data", "err", err)
+		slog.Error("failed parsing request data", "err", err)
 		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	req.Ip = remoteip.Get(r)
 	req.Ua = r.UserAgent()
-	ctx := r.Context()
+	m := metrics.Get(ctx)
+
 	if !accept(ctx, req.D) {
-		w.WriteHeader(http.StatusNotFound)
+		m.Events.Rejected.Inc()
+		w.Header().Set("x-vince-dropped", "1")
+		w.WriteHeader(http.StatusOK)
 		return
 	}
+	m.Events.Accepted.Inc()
 	worker.Submit(ctx, req)
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("ok"))
@@ -73,11 +57,12 @@ func accept(ctx context.Context, domain string) (ok bool) {
 
 // SendEvent accepts analytics event. Assumes req has already been validated
 func (a *API) SendEvent(ctx context.Context, req *apiv1.Event) (*emptypb.Empty, error) {
+	m := metrics.Get(ctx)
 	if !accept(ctx, req.D) {
-		RejectedEvents.Inc()
+		m.Events.Rejected.Inc()
 		return &emptypb.Empty{}, nil
 	}
-	AcceptedEvents.Inc()
+	m.Events.Accepted.Inc()
 	worker.Submit(ctx, req)
 	return nil, nil
 }
