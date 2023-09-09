9bcb6f41cc5cc3f04aa1bcb3262d9e6248daa64a
gernest <geofreyernest@live.com>
2023-09-09 11:25:54 +0300

    a2: implement Jwt AuthorizeTokenGen

--- internal/a2/jwt.go
+++ internal/a2/jwt.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/oklog/ulid/v2"
-	"github.com/vinceanalytics/vince/internal/core"
 	"github.com/vinceanalytics/vince/internal/scopes"
 	"github.com/vinceanalytics/vince/internal/secrets"
 	"github.com/vinceanalytics/vince/internal/tokens"
@@ -14,6 +13,7 @@ import (
 type JWT struct{}
 
 var _ AccessTokenGen = (*JWT)(nil)
+var _ AuthorizeTokenGen = (*JWT)(nil)
 
 func (JWT) GenerateAccessToken(ctx context.Context, data *AccessData, generaterefresh bool) (accesstoken string, refreshtoken string, err error) {
 	priv := secrets.Get(ctx)
@@ -26,7 +26,7 @@ func (JWT) GenerateAccessToken(ctx context.Context, data *AccessData, generatere
 			data.Client.GetId(),
 		},
 		ExpiresAt: jwt.NewNumericDate(data.ExpireAt()),
-		IssuedAt:  jwt.NewNumericDate(core.Now(ctx)),
+		IssuedAt:  jwt.NewNumericDate(data.CreatedAt),
 	})
 
 	accesstoken, err = token.SignedString(priv)
@@ -43,11 +43,27 @@ func (JWT) GenerateAccessToken(ctx context.Context, data *AccessData, generatere
 		Audience: jwt.ClaimStrings{
 			data.Client.GetId(),
 		},
-		IssuedAt: jwt.NewNumericDate(core.Now(ctx)),
+		IssuedAt: jwt.NewNumericDate(data.CreatedAt),
 	})
 	refreshtoken, err = token.SignedString(priv)
 	if err != nil {
 		return "", "", err
 	}
 	return
+}
+
+func (JWT) GenerateAuthorizeToken(ctx context.Context, data *AuthorizeData) (string, error) {
+	priv := secrets.Get(ctx)
+	me := tokens.GetAccount(ctx)
+	token := jwt.NewWithClaims(&jwt.SigningMethodEd25519{}, &jwt.RegisteredClaims{
+		Issuer:  scopes.ResourceBaseURL,
+		Subject: me.Name,
+		ID:      ulid.Make().String(),
+		Audience: jwt.ClaimStrings{
+			data.Client.GetId(),
+		},
+		ExpiresAt: jwt.NewNumericDate(data.ExpireAt()),
+		IssuedAt:  jwt.NewNumericDate(data.CreatedAt),
+	})
+	return token.SignedString(priv)
 }
