0f86d51387a4d99c1bbb51588018a200759fa9be
gernest <geofreyernest@live.com>
2024-02-02 14:27:07 +0300

    parse event

--- event.go
+++ event.go
@@ -1,8 +1,19 @@
 package staples
 
 import (
+	"context"
+	"net"
+	"net/url"
+	"strings"
 	"sync"
 	"time"
+
+	"github.com/cespare/xxhash/v2"
+	v1 "github.com/vinceanalytics/staples/gen/go/staples/v1"
+	"github.com/vinceanalytics/staples/geo"
+	"github.com/vinceanalytics/staples/logger"
+	"github.com/vinceanalytics/staples/ref"
+	"github.com/vinceanalytics/staples/ua"
 )
 
 type Event struct {
@@ -17,30 +28,30 @@ type Event struct {
 	//
 	// NOTE: Bounce is calculated per session. We simply want to know if a user
 	// stayed and browsed the website.
-	Bounce          int64
-	Session         int64
-	Duration        float64
-	Browser         string
-	Browser_Version string
-	City            string
-	Country         string
-	Domain          string
-	Entry_Page      string
-	Exit_Page       string
-	Host            string
-	Event           string
-	Os              string
-	OsVersion       string
-	Path            string
-	Referrer        string
-	Referrer_Source string
-	Region          string
-	Screen          string
-	Utm_Campaign    string
-	Utm_Content     string
-	Utm_Medium      string
-	Utm_Source      string
-	Utm_Term        string
+	Bounce         int64
+	Session        int64
+	Duration       float64
+	Browser        string
+	BrowserVersion string
+	City           string
+	Country        string
+	Domain         string
+	EntryPage      string
+	ExitPage       string
+	Host           string
+	Event          string
+	Os             string
+	OsVersion      string
+	Path           string
+	Referrer       string
+	ReferrerSource string
+	Region         string
+	Screen         string
+	UtmCampaign    string
+	UtmContent     string
+	UtmMedium      string
+	UtmSource      string
+	UtmTerm        string
 }
 
 var eventsPool = &sync.Pool{New: func() any { return new(Event) }}
@@ -57,7 +68,7 @@ func (e *Event) Release() {
 func (e *Event) TS() int64 { return e.Timestamp }
 
 func (e *Event) Hit() {
-	e.Entry_Page = e.Path
+	e.EntryPage = e.Path
 	e.Bounce = 1
 	e.Session = 1
 }
@@ -69,8 +80,118 @@ func (s *Event) Update(e *Event) {
 		s.Bounce, e.Bounce = 0, 0
 	}
 	e.Session = 0
-	e.Exit_Page = e.Path
+	e.ExitPage = e.Path
 	// Track duration since last visit.
 	e.Duration = time.UnixMilli(e.Timestamp).Sub(time.UnixMilli(s.Timestamp)).Seconds()
 	s.Timestamp = e.Timestamp
+}
+
+func Parse(ctx context.Context, req *v1.Event) *Event {
+	log := logger.Get(ctx)
+	if req.Url == "" || req.N == "" || req.D == "" {
+		log.Error("invalid request")
+		return nil
+	}
+	uri, err := url.Parse(req.Url)
+	if err != nil {
+		log.Error("failed parsing event url", "err", err)
+		return nil
+	}
+	if uri.Scheme == "data" {
+		log.Error("Data url scheme is not supported")
+		return nil
+	}
+	host := sanitizeHost(uri.Host)
+	query := uri.Query()
+
+	ref, src, err := refSource(query, req.R)
+	if err != nil {
+		log.Error("failed parsing referer", "err", err)
+		return nil
+	}
+	path := uri.Path
+	if len(path) > 2000 {
+		log.Error("Path too long", "path", path)
+		return nil
+	}
+	if req.H && uri.Fragment != "" {
+		path += "#" + uri.Fragment
+	}
+	domain := req.D
+	agent := ua.Get(req.Ua)
+	var city geo.Info
+	if req.Ip != "" {
+		ip := net.ParseIP(req.Ip)
+		city = geo.Get(ctx).Get(ip)
+	}
+	var screenSize string
+	switch {
+	case req.W < 576:
+		screenSize = "mobile"
+	case req.W < 992:
+		screenSize = "tablet"
+	case req.W < 1440:
+		screenSize = "laptop"
+	case req.W >= 1440:
+		screenSize = "desktop"
+	}
+	userID := Fingerprint(req.Ip, req.Ua, domain, host)
+	e := NewEvent()
+	e.ID = int64(userID)
+	e.Event = req.N
+	e.Host = host
+	e.Path = path
+	e.Domain = domain
+	e.UtmMedium = query.Get("utm_medium")
+	e.UtmSource = query.Get("utm_source")
+	e.UtmCampaign = query.Get("utm_campaign")
+	e.UtmContent = query.Get("utm_content")
+	e.UtmTerm = query.Get("utm_term")
+	e.Os = agent.Os
+	e.OsVersion = agent.OsVersion
+	e.Browser = agent.Browser
+	e.BrowserVersion = agent.BrowserVersion
+	e.ReferrerSource = src
+	e.Referrer = ref
+	e.Country = city.Country
+	e.Region = city.Region
+	e.City = city.City
+	e.Screen = screenSize
+	e.Timestamp = req.Timestamp.AsTime().UnixMilli()
+	return e
+}
+
+func sanitizeHost(s string) string {
+	return strings.TrimPrefix(strings.TrimSpace(s), "www.")
+}
+
+func refSource(q url.Values, u string) (xref, source string, err error) {
+	r, err := url.Parse(u)
+	if err != nil {
+		return "", "", err
+	}
+	r.Host = sanitizeHost(r.Host)
+	r.Path = strings.TrimSuffix(r.Path, "/")
+	xref = r.String()
+	source = q.Get("utm_source")
+	if source == "" {
+		source = q.Get("source")
+	}
+	if source == "" {
+		source = q.Get("ref")
+	}
+	if source != "" {
+		return
+	}
+	source = ref.Search(r.Host)
+	return
+}
+func Fingerprint(remoteIP, userAgent, domain, host string) (sum uint64) {
+	var h xxhash.Digest
+	h.WriteString(remoteIP)
+	h.WriteString(userAgent)
+	h.WriteString(domain)
+	h.WriteString(host)
+	sum = h.Sum64()
+	return
 }
