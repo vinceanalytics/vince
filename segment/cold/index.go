1778f3f82129d16f2397c57f76d0ac7a9b8b07b6
gernest <geofreyernest@live.com>
2024-02-10 04:18:38 +0300

    Revert "add cold.Index"
    This reverts commit 5f06c000fa520fdd33e73e0b2f238c4a22e92086.

--- segment/cold/index.go
+++ segment/cold/index.go
@@ -15,20 +15,12 @@ var skipFields = map[string]struct{}{
 	camel.Case(v1.Filters_Duration.String()): {},
 }
 
-type Index struct {
-	segment.Segment
-	DiskSize uint64
-}
-
-func New(r arrow.Record) (*Index, error) {
+func New(r arrow.Record) (segment.Segment, error) {
 	document := NewRecord(r, func(r arrow.Record, i int) bool {
 		_, ok := skipFields[r.ColumnName(i)]
 		return !ok
 	})
 	defer document.Release()
-	seg, size, err := ice.New([]segment.Document{document}, func(s string, i int) float32 { return 0 })
-	if err != nil {
-		return nil, err
-	}
-	return &Index{Segment: seg, DiskSize: size}, nil
+	seg, _, err := ice.New([]segment.Document{document}, func(s string, i int) float32 { return 0 })
+	return seg, err
 }
