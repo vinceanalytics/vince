c59822f879d0128e73ad8d8c81dfbf02bd7f7f91
gernest <geofreyernest@live.com>
2024-02-13 13:49:06 +0300

    load: send multiple page view events

--- load/testdata/page_view.js
+++ load/testdata/page_view.js
@@ -1,3 +1,5 @@
-const session = createSession("http://vinceanalytics.com");
-
-session.Send("pageview", "/");
+const site = "https://vinceanalytics.com"
+for (let index = 0; index < limit; index++) {
+    const session = createSession(site);
+    session.Send("pageview", "/");
+}
