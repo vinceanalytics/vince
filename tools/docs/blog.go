618f2528d98cb79250c6cbea6911766ae9f3b77e
gernest <geofreyernest@live.com>
2024-02-14 21:31:22 +0300

    docs: add author to blog post

--- tools/docs/blog.go
+++ tools/docs/blog.go
@@ -306,7 +306,7 @@ func renderPost(text []byte) (o Post) {
 	})
 
 	ast.Walk(func(node *blackfriday.Node, entering bool) blackfriday.WalkStatus {
-		if node.Type == blackfriday.Heading && node.HeadingData.Level == 6 {
+		if node.Type == blackfriday.Heading && node.HeadingData.Level == 1 || node.HeadingData.Level == 6 {
 			return blackfriday.SkipChildren
 		}
 		return r.RenderNode(w, node, entering)
