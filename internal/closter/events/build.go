12ca2b932768afdd2dd9230a1b01b9b60b38f9de
gernest <geofreyernest@live.com>
2024-02-23 00:31:27 +0300

    sort by timestamp before writing

--- internal/closter/events/build.go
+++ internal/closter/events/build.go
@@ -62,6 +62,9 @@ func (b *Builder) Release() {
 
 func (b *Builder) Write(list *v1.List) arrow.Record {
 	ls := list.GetItems()
+	sort.Slice(ls, func(i, j int) bool {
+		return ls[i].Timestamp < ls[j].Timestamp
+	})
 	b.r.Reserve(len(ls))
 	for _, e := range ls {
 		b.writeData(e)
