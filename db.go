b7fdd17ca691a827d0db19ef26365e923a8def8f
gernest <geofreyernest@live.com>
2024-07-23 13:22:55 +0300

    add save label

--- db.go
+++ db.go
@@ -1,9 +1,64 @@
 package len64
 
 import (
-	"github.com/cockroachdb/pebble"
+	"context"
+	"log/slog"
+	"time"
+
+	v1 "github.com/gernest/len64/gen/go/len64/v1"
+)
+
+const (
+	timestampField = "_timestamp"
+	dateField      = "_date"
 )
 
 type DB struct {
-	db *pebble.DB
+	store *Store[*v1.Model]
+	tasks chan *task
+}
+
+func (db *DB) Start(ctx context.Context) error {
+	b, err := db.store.Batch()
+	if err != nil {
+		return err
+	}
+	go db.startBatch(b, ctx)
+	return nil
+}
+
+func (db *DB) startBatch(b *Batch[*v1.Model], ctx context.Context) {
+	ts := time.NewTicker(time.Minute)
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case e := <-db.tasks:
+			err := b.Write(e.model, func(idx Index) {
+				for k, v := range e.meta {
+					idx.String(k, v)
+				}
+				idx.Int64(timestampField, int64(e.model.Timestamp))
+				idx.Int64(dateField, date(e.model.Timestamp))
+			})
+			if err != nil {
+				slog.Error("writing model", "err", err)
+			}
+		case <-ts.C:
+			err := b.Flush()
+			if err != nil {
+				slog.Error("flushing batch", "err", err)
+			}
+		}
+	}
+}
+
+func date(ts uint64) int64 {
+	yy, mm, dd := time.UnixMilli(int64(ts)).Date()
+	return time.Date(yy, mm, dd, 0, 0, 0, 0, time.UTC).UnixMilli()
+}
+
+type task struct {
+	model *v1.Model
+	meta  map[string]string
 }
