a40deba213baabaa8f8d6e91f0d608c9349d88f9
gernest <geofreyernest@live.com>
2024-07-31 19:26:32 +0300

    add rate limiting for domains

--- go.mod
+++ go.mod
@@ -15,6 +15,7 @@ require (
 	github.com/oschwald/geoip2-golang v1.11.0
 	github.com/stretchr/testify v1.9.0
 	golang.org/x/crypto v0.25.0
+	golang.org/x/time v0.3.0
 	google.golang.org/protobuf v1.34.2
 	gopkg.in/yaml.v2 v2.4.0
 )
--- internal/domains/domains_cache.go
+++ internal/domains/domains_cache.go
@@ -3,31 +3,48 @@ package domains
 import (
 	"hash/crc32"
 	"sync"
+	"time"
 
 	"github.com/RoaringBitmap/roaring/v2"
 	"github.com/gernest/len64/internal/kv"
+	"golang.org/x/time/rate"
 )
 
 type Cache struct {
 	b  roaring.Bitmap
+	r  map[uint32]*rate.Limiter
 	mu sync.RWMutex
 }
 
 func New(db kv.KeyValue) (*Cache, error) {
-	c := new(Cache)
+	c := &Cache{r: make(map[uint32]*rate.Limiter)}
 	h := crc32.NewIEEE()
+	limit := limit()
 	return c, kv.Domains(db, func(domain string) {
 		h.Reset()
 		h.Write([]byte(domain))
-		c.b.Add(h.Sum32())
+		id := h.Sum32()
+		c.b.Add(id)
+		// We are strict with quota, zero burst given!
+		c.r[id] = rate.NewLimiter(rate.Limit(limit), 0)
 	})
+}
+
+// We limit up to 100 hits per site per day to ensure smooth operation but with
+// enough traffic to stress test deployments.
+//
+// We don't care if it is bots or legit traffic, so long it is coming from a
+// registered site we count it as a hit.
+func limit() float64 {
+	return float64(100) / (24 * time.Hour).Seconds()
 }
 
 func (c *Cache) Contains(domain string) (ok bool) {
 	h := crc32.NewIEEE()
 	h.Write([]byte(domain))
+	id := h.Sum32()
 	c.mu.RLock()
-	ok = c.b.Contains(h.Sum32())
+	ok = c.b.Contains(id) && c.r[id].Allow()
 	c.mu.RUnlock()
 	return
 }
@@ -35,15 +52,19 @@ func (c *Cache) Contains(domain string) (ok bool) {
 func (c *Cache) Add(domain string) {
 	h := crc32.NewIEEE()
 	h.Write([]byte(domain))
+	id := h.Sum32()
 	c.mu.Lock()
-	c.b.Add(h.Sum32())
+	c.b.Add(id)
+	c.r[id] = rate.NewLimiter(rate.Limit(limit()), 0)
 	c.mu.Unlock()
 }
 
 func (c *Cache) Remove(domain string) {
 	h := crc32.NewIEEE()
 	h.Write([]byte(domain))
+	id := h.Sum32()
 	c.mu.Lock()
-	c.b.Remove(h.Sum32())
+	c.b.Remove(id)
+	delete(c.r, id)
 	c.mu.Unlock()
 }
