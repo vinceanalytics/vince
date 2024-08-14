6142fd75ef96a03e33aa9daca188171308be762b
gernest <geofreyernest@live.com>
2024-08-15 02:52:27 +0300

    search all shards for data.

--- internal/ro2/search.go
+++ internal/ro2/search.go
@@ -71,6 +71,10 @@ func (o *Proto[T]) Select(
 	domainID := uint64(hash.Sum32())
 
 	return o.View(func(tx *Tx) error {
+
+		// We iterate on shards in reverse. We are always interested in latest data. This way we can have early exit when the caller is done but we have more shards left.
+		slices.Reverse(shards)
+
 		for i := range shards {
 			shard := shards[i]
 
