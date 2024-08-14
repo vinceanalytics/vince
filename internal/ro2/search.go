2e11239920294b7b6e38fef1c0b3ec00a07bb37f
gernest <geofreyernest@live.com>
2024-08-14 21:25:04 +0300

    initial select implementation

---
+++ internal/ro2/search.go
@@ -0,0 +1,64 @@
+package ro2
+
+import (
+	"hash/crc32"
+	"time"
+
+	"github.com/vinceanalytics/vince/internal/ro"
+	"github.com/vinceanalytics/vince/internal/roaring/roaring64"
+)
+
+// all fields
+const (
+	domainField uint64 = 13
+)
+
+func (o *Proto[T]) Select(start, end int64, domain string,
+	f func(tx *Tx, date, shard uint64, match *roaring64.Bitmap) error) error {
+	dates := ro.DateRange(
+		time.UnixMilli(start).UTC(),
+		time.UnixMilli(end).UTC(),
+	)
+	if len(dates) == 0 {
+		return nil
+	}
+
+	shards := o.shards()
+
+	hash := crc32.NewIEEE()
+	hash.Write([]byte(domain))
+	domainID := uint64(hash.Sum32())
+
+	return o.View(func(tx *Tx) error {
+		// processing is done per shard per day
+		for i := range shards {
+			shard := shards[i]
+
+			for j := range dates {
+				date := dates[j]
+
+				b := tx.Row(date, shard, domainField, domainID)
+				if b.IsEmpty() {
+					continue
+				}
+
+				// found a match
+				err := f(tx, date, shard, b)
+				if err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	})
+}
+
+// compute possible shards based on current id
+func (o *Proto[T]) shards() []uint64 {
+	shard := o.seq.Load() / ro.ShardWidth
+	n := make([]uint64, 0, shard+1)
+	for i := uint64(0); i <= shard; i++ {
+		n = append(n, i)
+	}
+	return n
+}
