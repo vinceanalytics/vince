193c6678613224ea33bfdd608d3a4ee6d8de3f1d
Geofrey Ernest <geofreyernest@live.com>
2024-09-17 00:35:48 +0300

    compute current visitors based on MINUTE time quantum

--- internal/ro2/current_visitors.go
+++ internal/ro2/current_visitors.go
@@ -1,32 +1,44 @@
 package ro2
 
 import (
-	"errors"
-	"io"
 	"time"
 
 	"github.com/vinceanalytics/vince/internal/alicia"
+	"github.com/vinceanalytics/vince/internal/ro"
 	"github.com/vinceanalytics/vince/internal/roaring/roaring64"
 )
 
 func (o *Store) CurrentVisitors(domain string) (visitors uint64, err error) {
-	end := time.Now().UTC()
-	start := end.Add(5 * time.Minute)
+	end := time.Now().UTC().Truncate(time.Minute)
+	// we want the currenttime to be included in the search
+	end = end.Add(time.Minute)
+	start := end.Add(-5 * time.Minute)
+
+	shard := o.seq.Load() / ro.ShardWidth
+
+	dom := NewEq(uint64(alicia.DOMAIN), domain)
 	r := roaring64.New()
-	err = o.Select(
-		start.UnixMilli(), end.UnixMilli(), domain, nil,
-		func(tx *Tx, shard uint64, match *roaring64.Bitmap) error {
-			tx.ExtractBSI(shard, uint64(alicia.ID), match, func(row uint64, c int64) {
-				r.Add(uint64(c))
-			})
-			// Only process a single shard. It doesn't matter if this query span more
-			// than a single shard.
-			return io.EOF
-		},
-	)
-	if errors.Is(err, io.EOF) {
-		err = nil
-	}
+
+	err = o.View(func(tx *Tx) error {
+		b := dom.match(tx, shard)
+		if b.IsEmpty() {
+			return nil
+		}
+		ts := tx.Cmp(uint64(alicia.MINUTE), shard, roaring64.RANGE,
+			start.UnixMilli(), end.UnixMilli(),
+		)
+		if ts.IsEmpty() {
+			return nil
+		}
+		b.And(ts)
+		if b.IsEmpty() {
+			return nil
+		}
+		tx.ExtractBSI(shard, uint64(alicia.ID), b, func(row uint64, c int64) {
+			r.Add(uint64(c))
+		})
+		return nil
+	})
 	visitors = r.GetCardinality()
 	return
 }
