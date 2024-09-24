4cf66233bef9be5471de810d79cfa75505853577
Geofrey Ernest <geofreyernest@live.com>
2024-09-24 09:08:30 +0300

    initial field time views

---
+++ internal/rbf/quantum/views.go
@@ -0,0 +1,88 @@
+package quantum
+
+import (
+	"fmt"
+	"strconv"
+	"sync"
+	"time"
+)
+
+type Field struct {
+	buffer [10]byte
+	ts     [4][]byte
+	full   []byte
+}
+
+var fieldPool = sync.Pool{New: func() any {
+	return new(Field)
+}}
+
+func NewField() *Field {
+	f := fieldPool.Get().(*Field)
+	return f
+}
+
+func (f *Field) Views(name string, fn func(view string) error) error {
+	f.full = append(f.full[:0], []byte(name)...)
+	f.full = append(f.full, '_')
+	n := len(f.full)
+	for i := range f.ts {
+		f.full = append(f.full[:n], f.ts[i]...)
+		err := fn(string(f.full))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (f *Field) Reset() {
+	clear(f.buffer[:])
+	for i := range f.ts {
+		clear(f.ts[i])
+	}
+	clear(f.full)
+	f.full = f.full[:0]
+}
+
+func (f *Field) Release() {
+	f.Reset()
+	fieldPool.Put(f)
+}
+
+const defaultQuantum = "YMDH"
+
+func (f *Field) ViewsByTimeInto(t time.Time) {
+	fullBuf := f.buffer
+	l := len(fullBuf) - 10
+	date := fullBuf[l : l+10]
+	y, m, d := t.Date()
+	h := t.Hour()
+	// Did you know that Sprintf, Printf, and other things like that all
+	// do allocations, and that doing allocations in a tight loop like this
+	// is stunningly expensive? viewsByTime was 25% of an ingest test's
+	// total CPU, not counting the garbage collector overhead. This is about
+	// 3%. No, I'm not totally sure that justifies it.
+	if y < 1000 {
+		ys := fmt.Sprintf("%04d", y)
+		copy(date[0:4], []byte(ys))
+	} else if y >= 10000 {
+		// This is probably a bad answer but there isn't really a
+		// good answer.
+		ys := fmt.Sprintf("%04d", y%1000)
+		copy(date[0:4], []byte(ys))
+	} else {
+		strconv.AppendInt(date[:0], int64(y), 10)
+	}
+	date[4] = '0' + byte(m/10)
+	date[5] = '0' + byte(m%10)
+	date[6] = '0' + byte(d/10)
+	date[7] = '0' + byte(d%10)
+	date[8] = '0' + byte(h/10)
+	date[9] = '0' + byte(h%10)
+	for i, unit := range defaultQuantum {
+		if int(unit) < len(lengthsByQuantum) && lengthsByQuantum[unit] != 0 {
+			f.ts[i] = append(f.ts[i][:0], fullBuf[:l+lengthsByQuantum[unit]]...)
+		}
+	}
+}
---
+++ internal/rbf/quantum/views_test.go
@@ -0,0 +1,43 @@
+package quantum
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestViews(t *testing.T) {
+	f := NewField()
+	defer f.Release()
+	ts := time.Date(2000, time.January, 2, 3, 4, 5, 6, time.UTC)
+	f.ViewsByTimeInto(ts)
+	var got []string
+	require.NoError(t, f.Views("F", func(view string) error {
+		got = append(got, view)
+		return nil
+	}))
+	require.Equal(t, []string{"F_2000", "F_200001", "F_20000102", "F_2000010203"}, got)
+}
+
+func BenchmarkField_ViewsByTimeInto(b *testing.B) {
+	f := NewField()
+	defer f.Release()
+	ts := time.Date(2000, time.January, 2, 3, 4, 5, 6, time.UTC)
+
+	for range b.N {
+		f.ViewsByTimeInto(ts)
+	}
+}
+func BenchmarkField_Views(b *testing.B) {
+	f := NewField()
+	defer f.Release()
+	ts := time.Date(2000, time.January, 2, 3, 4, 5, 6, time.UTC)
+	f.ViewsByTimeInto(ts)
+
+	for range b.N {
+		f.Views("F", func(view string) error {
+			return nil
+		})
+	}
+}
