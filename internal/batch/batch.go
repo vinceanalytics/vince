b2073e8ea2746b3421c98fd19a14a6bda8b1156f
Geofrey Ernest <geofreyernest@live.com>
2024-10-01 14:49:52 +0300

    use sroar for batch

--- internal/batch/batch.go
+++ internal/batch/batch.go
@@ -4,14 +4,13 @@ import (
 	"bytes"
 	"errors"
 	"fmt"
-	"sync"
 	"time"
 
-	"github.com/RoaringBitmap/roaring/v2/roaring64"
 	"github.com/dgraph-io/badger/v4"
 	v1 "github.com/vinceanalytics/vince/gen/go/vince/v1"
 	"github.com/vinceanalytics/vince/internal/compute"
 	"github.com/vinceanalytics/vince/internal/encoding"
+	"github.com/vinceanalytics/vince/internal/roaring"
 	"google.golang.org/protobuf/reflect/protoreflect"
 )
 
@@ -21,13 +20,15 @@ type KV interface {
 }
 
 type Batch struct {
-	data    map[encoding.Key]*roaring64.BSI
+	data    map[encoding.Key]*roaring.BSI
 	domains map[string]uint64
+	offsets []uint32
+	buffer  []byte
 }
 
 func NewBatch() *Batch {
 	return &Batch{
-		data:    make(map[encoding.Key]*roaring64.BSI),
+		data:    make(map[encoding.Key]*roaring.BSI),
 		domains: make(map[string]uint64),
 	}
 }
@@ -49,7 +50,7 @@ func (b *Batch) Add(tx KV, m *v1.Model) error {
 		}
 		bs, ok := b.data[key]
 		if !ok {
-			bs = roaring64.NewDefaultBSI()
+			bs = roaring.NewDefaultBSI()
 			b.data[key] = bs
 		}
 		var value int64
@@ -83,6 +84,10 @@ func (b *Batch) Save(db *badger.DB) (err error) {
 			err = tx.Commit()
 		}
 		clear(b.data)
+		clear(b.offsets)
+		clear(b.buffer)
+		b.offsets = b.offsets[:0]
+		b.buffer = b.buffer[:0]
 	}()
 	for k, v := range b.data {
 		err = b.saveTs(tx, k, v)
@@ -105,8 +110,7 @@ func (b *Batch) Save(db *badger.DB) (err error) {
 	return nil
 }
 
-func (b *Batch) saveTs(tx *badger.Txn, key encoding.Key, value *roaring64.BSI) error {
-	value.RunOptimize()
+func (b *Batch) saveTs(tx *badger.Txn, key encoding.Key, value *roaring.BSI) error {
 	ts := time.UnixMilli(int64(key.Time)).UTC()
 	return errors.Join(
 		b.saveKey(tx, encoding.Key{Field: key.Field}, value),                   // global
@@ -135,7 +139,7 @@ func month(ts time.Time) uint64 {
 	return uint64(compute.Month(ts).UnixMilli())
 }
 
-func (b *Batch) saveKey(tx *badger.Txn, key encoding.Key, value *roaring64.BSI) error {
+func (b *Batch) saveKey(tx *badger.Txn, key encoding.Key, value *roaring.BSI) error {
 	return b.save(
 		tx,
 		encoding.EncodeKey(key),
@@ -143,46 +147,22 @@ func (b *Batch) saveKey(tx *badger.Txn, key encoding.Key, value *roaring64.BSI) 
 	)
 }
 
-func (b *Batch) save(tx *badger.Txn, key []byte, value *roaring64.BSI) error {
+func (b *Batch) save(tx *badger.Txn, key []byte, value *roaring.BSI) error {
 	it, err := tx.Get(key)
 	if err != nil {
-		if errors.Is(err, badger.ErrKeyNotFound) {
-			b := get()
-			defer put(b)
-			_, err := value.WriteTo(b)
-			if err != nil {
-				return err
-			}
-			return tx.Set(key, bytes.Clone(b.Bytes()))
+		if !errors.Is(err, badger.ErrKeyNotFound) {
+			return err
 		}
-		return err
+		b.offsets, b.buffer = value.ToBufferWith(b.offsets, b.buffer)
+	} else {
+		err = it.Value(func(val []byte) error {
+			bs := roaring.NewBSIFromBuffer(val)
+			b.offsets, b.buffer = bs.Or(value).ToBufferWith(b.offsets, b.buffer)
+			return err
+		})
+		if err != nil {
+			return err
+		}
 	}
-	exist := roaring64.NewDefaultBSI()
-	err = it.Value(func(val []byte) error {
-		_, err := exist.ReadFrom(bytes.NewReader(val))
-		return err
-	})
-	if err != nil {
-		return err
-	}
-	exist.ParOr(0, value)
-	bs := get()
-	defer put(bs)
-	exist.RunOptimize()
-	_, err = exist.WriteTo(bs)
-	if err != nil {
-		return err
-	}
-	return tx.Set(key, bytes.Clone(bs.Bytes()))
-}
-
-var bufferPool = &sync.Pool{New: func() any { return new(bytes.Buffer) }}
-
-func get() *bytes.Buffer {
-	return bufferPool.Get().(*bytes.Buffer)
-}
-
-func put(b *bytes.Buffer) {
-	b.Reset()
-	bufferPool.Put(b)
+	return tx.Set(key, bytes.Clone(b.buffer))
 }
