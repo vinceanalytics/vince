a5cd47f4cc139e35b6f51976a34d208ab6da171e
Geofrey Ernest <geofreyernest@live.com>
2024-10-08 19:06:37 +0300

    use sroar for domains check

--- internal/domains/domains.go
+++ internal/domains/domains.go
@@ -3,28 +3,28 @@ package domains
 import (
 	"sync/atomic"
 
-	"github.com/RoaringBitmap/roaring/v2"
 	"github.com/dgraph-io/badger/v4/y"
 	v1 "github.com/vinceanalytics/vince/gen/go/vince/v1"
+	"github.com/vinceanalytics/vince/internal/roaring"
 )
 
 var domains = newDom()
 
 func newDom() *atomic.Pointer[roaring.Bitmap] {
 	var d atomic.Pointer[roaring.Bitmap]
-	d.Store(roaring.New())
+	d.Store(roaring.NewBitmap())
 	return &d
 }
 
 func Reload(l func(f func(*v1.Site))) {
-	keys := roaring.New()
+	keys := roaring.NewBitmap()
 	l(func(s *v1.Site) {
-		keys.Add(y.Hash([]byte(s.Domain)))
+		keys.Set(uint64(y.Hash([]byte(s.Domain))))
 	})
 	domains.Store(keys)
 }
 
 func Allow(domain string) bool {
 	return domains.Load().
-		Contains(y.Hash([]byte(domain)))
+		Contains(uint64(y.Hash([]byte(domain))))
 }
