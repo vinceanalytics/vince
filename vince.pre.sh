7c531ad92a44eefd0529d133f7b8000e28712de7
Geofrey Ernest <geofreyernest@live.com>
2024-11-20 19:30:01 +0300

    fix debian package

--- vince.pre.sh
+++ vince.pre.sh
@@ -1,4 +1,1 @@
-getent passwd otelcol-otlp >/dev/null || useradd --system --user-group --no-create-home --shell /sbin/nologin vince
-
-mkdir -p /var/lib/vince-data
-chown -R vince:vince /var/lib/vince-data
+getent passwd vince >/dev/null || useradd --system --user-group --no-create-home --shell /sbin/nologin vince
